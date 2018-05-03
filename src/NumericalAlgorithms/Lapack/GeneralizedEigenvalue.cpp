921071e67325a701b1fdca174931d3d80152f913
Geoffrey Lovelace <geoffrey4444@gmail.com>
2018-05-03 16:04:52 -0700

    Update generalized eigenvalue

--- src/NumericalAlgorithms/Lapack/GeneralizedEigenvalue.cpp
+++ src/NumericalAlgorithms/Lapack/GeneralizedEigenvalue.cpp
@@ -16,20 +16,19 @@ extern void dggev_(char*, char*, int*, double*, int*, double*, int*, double*,
                    int*, int*);
 }
 
-namespace {
 void generalized_eigenvalue(const Matrix& A, const Matrix& B,
                             DataVector& eigenvalues_real_part,
                             DataVector& eigenvalues_imaginary_part,
                             Matrix& eigenvectors) noexcept {
   // Sanity checks on the sizes of the vectors and matrices
   const size_t number_of_rows = A.rows();
-  static_assert(number_of_rows == A.columns(), "Matrix A should be square");
-  static_assert(number_of_rows == B.rows() && number_of_rows == B.columns(),
-                "Matrix A and matrix B should be the same size");
-  static_assert(number_of_rows == eigenvectors.rows() &&
+  ASSERT(number_of_rows == A.columns(), "Matrix A should be square");
+  ASSERT(number_of_rows == B.rows() && number_of_rows == B.columns(),
+         "Matrix A and matrix B should be the same size");
+  ASSERT(number_of_rows == eigenvectors.rows() &&
                     number_of_rows == eigenvectors.columns(),
                 "Matrix A and matrix eigenvectors should have the same size");
-  static_assert(number_of_rows == eigenvalues_real_part.size() &&
+  ASSERT(number_of_rows == eigenvalues_real_part.size() &&
                     number_of_rows == eigenvalues_imaginary_part.size(),
                 "eigenvalues DataVector sizes should equal number of columns "
                 "in Matrix A");
@@ -41,8 +40,15 @@ void generalized_eigenvalue(const Matrix& A, const Matrix& B,
   char compute_left_eigenvectors = 'N';
   char compute_right_eigenvectors = 'V';
 
+  // Lapack overwrites the input matrices, so copy the inputs into
+  // non-const matrices
+  Matrix A_for_lapack = A;
+  Matrix B_for_lapack = B;
+
   // Lapack expects the sizes to be ints, not size_t.
-  const int matrix_and_vector_size = static_cast<int>(number_of_rows);
+  // NOTE: not const because lapack function dggev_() arguments
+  // are not const.
+  int matrix_and_vector_size = static_cast<int>(number_of_rows);
 
   // Lapack splits the eigenvalues into unnormalized real and imaginary
   // parts, which it calls alphar and alphai, and a normalization,
@@ -54,31 +60,32 @@ void generalized_eigenvalue(const Matrix& A, const Matrix& B,
 
   // Lapack uses a work vector, that should have a size 8N
   // for doing eigenvalue problems with NxN matrices
-  const size_t work_size = number_of_rows * 8;
+  // Note: non-const int, not size_t, because lapack wants an non-cosnt int
+  int work_size = number_of_rows * 8;
   DataVector lapack_work(number_of_rows, 0.0);
 
   //  Lapack uses an integer called info to return its status
   //  info = 0 : success
   //  info = -i: ith argument had bad value
   //  info > 0: some other failure
-  const int info = 0;
+  int info = 0;
 
   dggev_(&compute_left_eigenvectors, &compute_right_eigenvectors,
-         &matrix_and_vector_size, A.data(), &matrix_and_vector_size, B.data(),
+         &matrix_and_vector_size, A_for_lapack.data(),
+         &matrix_and_vector_size, B_for_lapack.data(),
          &matrix_and_vector_size, unnormalized_eigenvalue_real_part.data(),
          unnormalized_eigenvalue_imaginary_part.data(),
          eigenvalue_normalization.data(), eigenvectors.data(),
          &matrix_and_vector_size, eigenvectors.data(), &matrix_and_vector_size,
-         lapack_work, &work_size, &info);
+         lapack_work.data(), &work_size, &info);
 
-  static_assert(info == 0, "Lapack failed to compute generalized eigenvectors");
+  ASSERT(info == 0, "Lapack failed to compute generalized eigenvectors");
 
   // compute the real and imaginary parts of the eigenvalues
   for (size_t i = 0; i < eigenvalues_real_part.size(); ++i) {
     eigenvalues_real_part[i] =
-        unnormalized_eigenvalue_real_part[i] / eigenvalue_normalization;
+        unnormalized_eigenvalue_real_part[i] / eigenvalue_normalization[i];
     eigenvalues_imaginary_part[i] =
-        unnormalized_eigenvalue_imaginary_part[i] / eigenvalue_normalization;
+        unnormalized_eigenvalue_imaginary_part[i] / eigenvalue_normalization[i];
   }
 }
-}  // namespace
