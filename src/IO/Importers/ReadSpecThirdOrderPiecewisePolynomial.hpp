519439eee17b2ca5cdd1ef31592be4a6033a2f1c
Geoffrey Lovelace <geoffrey4444@gmail.com>
2020-07-03 08:02:03 -0700

    Read correct deriv from spec piecewise polynomial

--- src/IO/Importers/ReadSpecThirdOrderPiecewisePolynomial.hpp
+++ src/IO/Importers/ReadSpecThirdOrderPiecewisePolynomial.hpp
@@ -153,7 +153,7 @@ struct ReadSpecThirdOrderPiecewisePolynomial {
           std::cout << "t = " << time_last_updated << "\n";
           for (size_t a = 0; a < number_of_components; ++a) {
             highest_derivative[a] =
-                dat_data(row, 4 + (max_deriv + 1) * a + max_deriv);
+                dat_data(row, 5 + (max_deriv + 1) * a + max_deriv);
           }
           spec_functions_of_time[spectre_name].update(time_last_updated,
                                                       highest_derivative);
