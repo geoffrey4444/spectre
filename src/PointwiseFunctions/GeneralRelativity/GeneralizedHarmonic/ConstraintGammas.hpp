4182fdb806b681f3026ac8570742e19c473557d2
Geoffrey Lovelace <geoffrey4444@gmail.com>
2020-08-06 15:06:18 -0700

    Hack: change hard-coded BH centers in damping params

--- src/PointwiseFunctions/GeneralRelativity/GeneralizedHarmonic/ConstraintGammas.hpp
+++ src/PointwiseFunctions/GeneralRelativity/GeneralizedHarmonic/ConstraintGammas.hpp
@@ -92,5 +92,139 @@ struct ConstraintGamma2Compute : ConstraintGamma2, db::ComputeTag {
 
   using base = ConstraintGamma2;
 };
+
+template <size_t SpatialDim, typename Frame>
+struct ConstraintGamma0BBHCompute : ConstraintGamma0, db::ComputeTag {
+  using argument_tags = tmpl::list<domain::Tags::Coordinates<SpatialDim, Frame>,
+                                   ::Tags::Time, domain::Tags::FunctionsOfTime>;
+
+  using volume_tags = tmpl::list<::Tags::Time, domain::Tags::FunctionsOfTime>;
+
+  using return_type = Scalar<DataVector>;
+
+  using FunctionsOfTime = std::unordered_map<
+      std::string, std::unique_ptr<domain::FunctionsOfTime::FunctionOfTime>>;
+
+  static constexpr void function(
+      const gsl::not_null<Scalar<DataVector>*> gamma,
+      const tnsr::I<DataVector, SpatialDim, Frame>& coords, const double time,
+      const FunctionsOfTime& functions_of_time) noexcept {
+    destructive_resize_components(gamma, get<0>(coords).size());
+
+    constexpr double m_A = 0.5;
+    constexpr double m_B = 0.5;
+    constexpr double x_A = 8.2997;
+    constexpr double x_B = -8.2997;
+
+    constexpr double width_A = 7.0 * m_A;
+    constexpr double width_B = 7.0 * m_B;
+    constexpr double width_O = 2.5 * (x_A - x_B);
+    constexpr double amp_A = 4.0 / m_A;
+    constexpr double amp_B = 4.0 / m_B;
+    constexpr double amp_O = 0.075 / (m_A + m_B);
+    constexpr double asymptotic_damping = 0.001 / (m_A + m_B);
+
+    // HACK: hard-code name of expansion factor as "ExpansionFactor"
+    const double inverse_expansion_factor =
+        1.0 / (functions_of_time.at("ExpansionFactor")->func(time)[0][0]);
+
+    auto distance_A_squared = make_with_value<Scalar<DataVector>>(coords, 0.0);
+    get(distance_A_squared) = square(get<0>(coords) - x_A) +
+                              square(get<1>(coords)) + square(get<2>(coords));
+    auto distance_B_squared = make_with_value<Scalar<DataVector>>(coords, 0.0);
+    get(distance_B_squared) = square(get<0>(coords) - x_B) +
+                              square(get<1>(coords)) + square(get<2>(coords));
+    auto distance_O_squared = make_with_value<Scalar<DataVector>>(coords, 0.0);
+    get(distance_O_squared) = get(dot_product(coords, coords));
+
+    get(*gamma) = amp_A * exp(-get(distance_A_squared) /
+                              square(width_A * inverse_expansion_factor)) +
+                  amp_B * exp(-get(distance_B_squared) /
+                              square(width_B * inverse_expansion_factor)) +
+                  amp_O * exp(-get(distance_O_squared) /
+                              square(width_O * inverse_expansion_factor)) +
+                  asymptotic_damping;
+  }
+
+  using base = ConstraintGamma0;
+};
+
+template <size_t SpatialDim, typename Frame>
+struct ConstraintGamma1BBHCompute : ConstraintGamma1, db::ComputeTag {
+  using argument_tags =
+      tmpl::list<domain::Tags::Coordinates<SpatialDim, Frame>>;
+
+  using return_type = Scalar<DataVector>;
+
+  static constexpr void function(
+      const gsl::not_null<Scalar<DataVector>*> gamma1,
+      const tnsr::I<DataVector, SpatialDim, Frame>& coords) noexcept {
+    destructive_resize_components(gamma1, get<0>(coords).size());
+    constexpr double amp = 0.999;
+    constexpr double x_A = 8.2997;
+    constexpr double x_B = -8.2997;
+
+    constexpr double width = 10.0 * (x_A - x_B);
+    get(*gamma1) =
+        amp * (-1.0 + exp(-get(dot_product(coords, coords)) / square(width)));
+  }
+
+  using base = ConstraintGamma1;
+};
+
+template <size_t SpatialDim, typename Frame>
+struct ConstraintGamma2BBHCompute : ConstraintGamma2, db::ComputeTag {
+  using argument_tags = tmpl::list<domain::Tags::Coordinates<SpatialDim, Frame>,
+                                   ::Tags::Time, domain::Tags::FunctionsOfTime>;
+
+  using volume_tags = tmpl::list<::Tags::Time, domain::Tags::FunctionsOfTime>;
+
+  using return_type = Scalar<DataVector>;
+
+  using FunctionsOfTime = std::unordered_map<
+      std::string, std::unique_ptr<domain::FunctionsOfTime::FunctionOfTime>>;
+
+  static constexpr void function(
+      const gsl::not_null<Scalar<DataVector>*> gamma,
+      const tnsr::I<DataVector, SpatialDim, Frame>& coords, const double time,
+      const FunctionsOfTime& functions_of_time) noexcept {
+    destructive_resize_components(gamma, get<0>(coords).size());
+    constexpr double m_A = 0.5;
+    constexpr double m_B = 0.5;
+    constexpr double x_A = 8.2997;
+    constexpr double x_B = -8.2997;
+
+    constexpr double width_A = 7.0 * m_A;
+    constexpr double width_B = 7.0 * m_B;
+    constexpr double width_O = 2.5 * (x_A - x_B);
+    constexpr double amp_A = 4.0 / m_A;
+    constexpr double amp_B = 4.0 / m_B;
+    constexpr double amp_O = 0.075 / (m_A + m_B);
+    constexpr double asymptotic_damping = 0.001 / (m_A + m_B);
+
+    // HACK: hard-code name of expansion factor as "ExpansionFactor"
+    const double inverse_expansion_factor =
+        1.0 / (functions_of_time.at("ExpansionFactor")->func(time)[0][0]);
+
+    auto distance_A_squared = make_with_value<Scalar<DataVector>>(coords, 0.0);
+    get(distance_A_squared) = square(get<0>(coords) - x_A) +
+                              square(get<1>(coords)) + square(get<2>(coords));
+    auto distance_B_squared = make_with_value<Scalar<DataVector>>(coords, 0.0);
+    get(distance_B_squared) = square(get<0>(coords) - x_B) +
+                              square(get<1>(coords)) + square(get<2>(coords));
+    auto distance_O_squared = make_with_value<Scalar<DataVector>>(coords, 0.0);
+    get(distance_O_squared) = get(dot_product(coords, coords));
+
+    get(*gamma) = amp_A * exp(-get(distance_A_squared) /
+                              square(width_A * inverse_expansion_factor)) +
+                  amp_B * exp(-get(distance_B_squared) /
+                              square(width_B * inverse_expansion_factor)) +
+                  amp_O * exp(-get(distance_O_squared) /
+                              square(width_O * inverse_expansion_factor)) +
+                  asymptotic_damping;
+  }
+
+  using base = ConstraintGamma2;
+};
 }  // namespace Tags
 }  // namespace GeneralizedHarmonic
