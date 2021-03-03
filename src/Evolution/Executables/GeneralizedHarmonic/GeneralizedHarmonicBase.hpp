2dd3495c0630072deb965c9b2cc004d6792ac670
Geoffrey Lovelace <geoffrey4444@gmail.com>
2021-03-03 14:08:17 -0800

    Add filtering

--- src/Evolution/Executables/GeneralizedHarmonic/GeneralizedHarmonicBase.hpp
+++ src/Evolution/Executables/GeneralizedHarmonic/GeneralizedHarmonicBase.hpp
@@ -49,6 +49,8 @@
 #include "NumericalAlgorithms/DiscontinuousGalerkin/BoundarySchemes/FirstOrder/FirstOrderScheme.hpp"
 #include "NumericalAlgorithms/DiscontinuousGalerkin/BoundarySchemes/FirstOrder/FirstOrderSchemeLts.hpp"
 #include "NumericalAlgorithms/DiscontinuousGalerkin/Tags.hpp"
+#include "NumericalAlgorithms/LinearOperators/ExponentialFilter.hpp"
+#include "NumericalAlgorithms/LinearOperators/FilterAction.hpp"
 #include "Options/Options.hpp"
 #include "Parallel/Actions/SetupDataBox.hpp"
 #include "Parallel/Actions/TerminatePhase.hpp"
@@ -325,7 +327,14 @@ struct GeneralizedHarmonicTemplateBase<EvolutionMetavarsDerived<
                                             derived_metavars>>,
                          tmpl::list<>>,
                      Actions::RecordTimeStepperData<>>>,
-      Actions::UpdateU<>>;
+      Actions::UpdateU<>,
+      dg::Actions::Filter<
+          Filters::Exponential<0>,
+          tmpl::list<
+              gr::Tags::SpacetimeMetric<volume_dim, Frame::Inertial,
+                                        DataVector>,
+              GeneralizedHarmonic::Tags::Pi<volume_dim, Frame::Inertial>,
+              GeneralizedHarmonic::Tags::Phi<volume_dim, Frame::Inertial>>>>;
 
   using initialization_actions = tmpl::list<
       Actions::SetupDataBox,
