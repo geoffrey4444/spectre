041d79769afb2d46328ee935e6480a209c3eec2f
Geoffrey Lovelace <geoffrey4444@gmail.com>
2021-06-06 16:47:00 -0700

    Enable filtering in GH exec

--- src/Evolution/Executables/GeneralizedHarmonic/EvolveGeneralizedHarmonic.hpp
+++ src/Evolution/Executables/GeneralizedHarmonic/EvolveGeneralizedHarmonic.hpp
@@ -66,6 +66,8 @@
 #include "NumericalAlgorithms/Interpolation/InterpolatorRegisterElement.hpp"
 #include "NumericalAlgorithms/Interpolation/Tags.hpp"
 #include "NumericalAlgorithms/Interpolation/TryToInterpolate.hpp"
+#include "NumericalAlgorithms/LinearOperators/ExponentialFilter.hpp"
+#include "NumericalAlgorithms/LinearOperators/FilterAction.hpp"
 #include "Options/Options.hpp"
 #include "Options/Protocols/FactoryCreation.hpp"
 #include "Parallel/Actions/SetupDataBox.hpp"
@@ -237,7 +239,16 @@ struct EvolutionMetavars {
       evolution::dg::Actions::ApplyBoundaryCorrections<EvolutionMetavars>,
       tmpl::conditional_t<
           local_time_stepping, tmpl::list<>,
-          tmpl::list<Actions::RecordTimeStepperData<>, Actions::UpdateU<>>>>;
+          tmpl::list<
+              Actions::RecordTimeStepperData<>, Actions::UpdateU<>,
+              dg::Actions::Filter<
+                  Filters::Exponential<0>,
+                  tmpl::list<gr::Tags::SpacetimeMetric<
+                                 volume_dim, Frame::Inertial, DataVector>,
+                             GeneralizedHarmonic::Tags::Pi<volume_dim,
+                                                           Frame::Inertial>,
+                             GeneralizedHarmonic::Tags::Phi<
+                                 volume_dim, Frame::Inertial>>>>>>;
 
   enum class Phase {
     Initialization,
