68822e60b8c58e1d25eb832d99679102e453e894
Geoffrey Lovelace <geoffrey4444@gmail.com>
2021-06-08 11:07:05 -0700

    Hack: ObserveNorms into ObserveFields

--- src/ParallelAlgorithms/Events/Factory.hpp
+++ src/ParallelAlgorithms/Events/Factory.hpp
@@ -8,17 +8,25 @@
 
 #include "ParallelAlgorithms/Events/ObserveErrorNorms.hpp"
 #include "ParallelAlgorithms/Events/ObserveFields.hpp"
+#include "ParallelAlgorithms/Events/ObserveNorms.hpp"
 #include "ParallelAlgorithms/Events/ObserveTimeStep.hpp"
 #include "Time/Actions/ChangeSlabSize.hpp"
 #include "Utilities/TMPL.hpp"
 
 namespace dg::Events {
+template <typename List>
+struct ExpandList;
+template <typename... Tags>
+struct ExpandList<tmpl::list<Tags...>> {
+  using type = ::Events::ObserveNorms<Tags...>;
+};
+
 template <size_t VolumeDim, typename TimeTag, typename Fields,
           typename SolutionFields>
 using field_observations = tmpl::flatten<tmpl::list<
     ObserveFields<VolumeDim, TimeTag, Fields, SolutionFields>,
     tmpl::conditional_t<std::is_same_v<SolutionFields, tmpl::list<>>,
-                        tmpl::list<>,
+                        typename ExpandList<Fields>::type,
                         ObserveErrorNorms<TimeTag, SolutionFields>>>>;
 }  // namespace dg::Events
 
