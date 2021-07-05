43f33148445869d0fcbd3e5f5d09a75893f558fd
Geoffrey Lovelace <geoffrey4444@gmail.com>
2021-07-05 07:22:09 -0700

    Hard-code time-dependent maps in Sphere

--- src/Domain/Creators/Sphere.cpp
+++ src/Domain/Creators/Sphere.cpp
@@ -13,11 +13,18 @@
 #include "Domain/CoordinateMaps/CoordinateMap.hpp"
 #include "Domain/CoordinateMaps/CoordinateMap.tpp"
 #include "Domain/CoordinateMaps/Equiangular.hpp"
+#include "Domain/CoordinateMaps/Identity.hpp"
 #include "Domain/CoordinateMaps/ProductMaps.hpp"
 #include "Domain/CoordinateMaps/ProductMaps.tpp"
+#include "Domain/CoordinateMaps/TimeDependent/CubicScale.hpp"
+#include "Domain/CoordinateMaps/TimeDependent/ProductMaps.hpp"
+#include "Domain/CoordinateMaps/TimeDependent/ProductMaps.tpp"
+#include "Domain/CoordinateMaps/TimeDependent/Rotation.hpp"
 #include "Domain/Creators/DomainCreator.hpp"  // IWYU pragma: keep
 #include "Domain/Domain.hpp"
 #include "Domain/DomainHelpers.hpp"
+#include "Domain/FunctionsOfTime/FixedSpeedCubic.hpp"
+#include "Domain/FunctionsOfTime/PiecewisePolynomial.hpp"
 #include "Domain/Structure/BlockNeighbor.hpp"  // IWYU pragma: keep
 #include "Utilities/ErrorHandling/Assert.hpp"
 #include "Utilities/MakeArray.hpp"
@@ -28,6 +35,22 @@ struct BlockLogical;  // IWYU pragma: keep
 }  // namespace Frame
 
 namespace domain::creators {
+void Sphere::check_for_parse_errors(const Options::Context& context) const {
+  using domain::BoundaryConditions::is_none;
+  if (is_none(boundary_condition_)) {
+    PARSE_ERROR(
+        context,
+        "None boundary condition is not supported. If you would like an "
+        "outflow boundary condition, you must use that.");
+  }
+  using domain::BoundaryConditions::is_periodic;
+  if (is_periodic(boundary_condition_)) {
+    PARSE_ERROR(context,
+                "Cannot have periodic boundary conditions with a Sphere");
+  }
+}
+
+// Time-independent constructor
 Sphere::Sphere(typename InnerRadius::type inner_radius,
                typename OuterRadius::type outer_radius,
                typename InitialRefinement::type initial_refinement,
@@ -44,19 +67,66 @@ Sphere::Sphere(typename InnerRadius::type inner_radius,
       initial_number_of_grid_points_(                        // NOLINT
           std::move(initial_number_of_grid_points)),         // NOLINT
       use_equiangular_map_(std::move(use_equiangular_map)),  // NOLINT
-      boundary_condition_(std::move(boundary_condition)) {
-  using domain::BoundaryConditions::is_none;
-  if (is_none(boundary_condition_)) {
-    PARSE_ERROR(
-        context,
-        "None boundary condition is not supported. If you would like an "
-        "outflow boundary condition, you must use that.");
-  }
-  using domain::BoundaryConditions::is_periodic;
-  if (is_periodic(boundary_condition_)) {
-    PARSE_ERROR(context,
-                "Cannot have periodic boundary conditions with a Sphere");
-  }
+      boundary_condition_(std::move(boundary_condition)),
+      enable_time_dependence_(false),
+      initial_time_(std::numeric_limits<double>::signaling_NaN()),
+      initial_expiration_delta_t_(std::numeric_limits<double>::signaling_NaN()),
+      expansion_map_outer_boundary_(
+          std::numeric_limits<double>::signaling_NaN()),
+      initial_expansion_(std::numeric_limits<double>::signaling_NaN()),
+      initial_expansion_velocity_(std::numeric_limits<double>::signaling_NaN()),
+      asymptotic_velocity_outer_boundary_(
+          std::numeric_limits<double>::signaling_NaN()),
+      decay_timescale_outer_boundary_velocity_(
+          std::numeric_limits<double>::signaling_NaN()),
+      expansion_function_of_time_name_({}),
+      initial_rotation_angle_(std::numeric_limits<double>::signaling_NaN()),
+      initial_angular_velocity_(std::numeric_limits<double>::signaling_NaN()),
+      rotation_about_z_axis_function_of_time_name_({}) {
+  check_for_parse_errors(context);
+}
+
+// Time-dependent constructor
+Sphere::Sphere(double initial_time,
+               std::optional<double> initial_expiration_delta_t,
+               double expansion_map_outer_boundary, double initial_expansion,
+               double initial_expansion_velocity,
+               double asymptotic_velocity_outer_boundary,
+               double decay_timescale_outer_boundary_velocity,
+               std::string expansion_function_of_time_name,
+               double initial_rotation_angle, double initial_angular_velocity,
+               std::string rotation_about_z_axis_function_of_time_name,
+               typename InnerRadius::type inner_radius,
+               typename OuterRadius::type outer_radius,
+               typename InitialRefinement::type initial_refinement,
+               typename InitialGridPoints::type initial_number_of_grid_points,
+               typename UseEquiangularMap::type use_equiangular_map,
+               std::unique_ptr<domain::BoundaryConditions::BoundaryCondition>
+                   boundary_condition,
+               const Options::Context& context)
+    : inner_radius_(std::move(inner_radius)),                // NOLINT
+      outer_radius_(std::move(outer_radius)),                // NOLINT
+      initial_refinement_(                                   // NOLINT
+          std::move(initial_refinement)),                    // NOLINT
+      initial_number_of_grid_points_(                        // NOLINT
+          std::move(initial_number_of_grid_points)),         // NOLINT
+      use_equiangular_map_(std::move(use_equiangular_map)),  // NOLINT
+      boundary_condition_(std::move(boundary_condition)),
+      enable_time_dependence_(true),
+      initial_time_(initial_time),
+      initial_expiration_delta_t_(initial_expiration_delta_t),
+      expansion_map_outer_boundary_(expansion_map_outer_boundary),
+      initial_expansion_(initial_expansion),
+      initial_expansion_velocity_(initial_expansion_velocity),
+      asymptotic_velocity_outer_boundary_(asymptotic_velocity_outer_boundary),
+      decay_timescale_outer_boundary_velocity_(
+          decay_timescale_outer_boundary_velocity),
+      expansion_function_of_time_name_(expansion_function_of_time_name),
+      initial_rotation_angle_(initial_rotation_angle),
+      initial_angular_velocity_(initial_angular_velocity),
+      rotation_about_z_axis_function_of_time_name_(
+          std::move(rotation_about_z_axis_function_of_time_name)) {
+  check_for_parse_errors(context);
 }
 
 Domain<3> Sphere::create_domain() const noexcept {
@@ -109,8 +179,61 @@ Domain<3> Sphere::create_domain() const noexcept {
     }
   }
 
-  return Domain<3>(std::move(coord_maps), corners, {},
-                   std::move(boundary_conditions_all_blocks));
+  Domain<3> domain{std::move(coord_maps),
+                   corners,
+                   {},
+                   std::move(boundary_conditions_all_blocks)};
+  if (enable_time_dependence_) {
+    // Note on frames: Because the relevant maps will all be composed before
+    // they are used, all maps here go from Frame::Grid (the frame after the
+    // final time-independent map is applied) to Frame::Inertial
+    // (the frame after the final time-dependent map is applied).
+    using CubicScaleMap = domain::CoordinateMaps::TimeDependent::CubicScale<3>;
+    using CubicScaleMapForComposition =
+        domain::CoordinateMap<Frame::Grid, Frame::Inertial, CubicScaleMap>;
+
+    using IdentityMap1D = domain::CoordinateMaps::Identity<1>;
+    using RotationMap2D = domain::CoordinateMaps::TimeDependent::Rotation<2>;
+    using RotationMap =
+        domain::CoordinateMaps::TimeDependent::ProductOf2Maps<RotationMap2D,
+                                                              IdentityMap1D>;
+    using RotationMapForComposition =
+        domain::CoordinateMap<Frame::Grid, Frame::Inertial, RotationMap>;
+
+    using CubicScaleAndRotationMapForComposition =
+        domain::CoordinateMap<Frame::Grid, Frame::Inertial, CubicScaleMap,
+                              RotationMap>;
+
+    constexpr size_t number_of_blocks{7};
+    std::vector<std::unique_ptr<
+        domain::CoordinateMapBase<Frame::Grid, Frame::Inertial, 3>>>
+        block_maps{number_of_blocks};
+
+    // All blocks get the same time-dependent maps: expansion and rotation
+    block_maps[number_of_blocks - 1] =
+        std::make_unique<CubicScaleAndRotationMapForComposition>(
+            domain::push_back(
+                CubicScaleMapForComposition{CubicScaleMap{
+                    expansion_map_outer_boundary_,
+                    expansion_function_of_time_name_,
+                    expansion_function_of_time_name_ + "OuterBoundary"s}},
+                RotationMapForComposition{RotationMap{
+                    RotationMap2D{rotation_about_z_axis_function_of_time_name_},
+                    IdentityMap1D{}}}));
+
+    // Fill in the rest of the block maps by cloning the relevant maps
+    for (size_t block = 0; block < number_of_blocks - 1; ++block) {
+      block_maps[block] = block_maps[number_of_blocks - 1]->get_clone();
+    }
+
+    // Finally, inject the time dependent maps into the corresponding blocks
+    for (size_t block = 0; block < number_of_blocks; ++block) {
+      domain.inject_time_dependent_map_for_block(block,
+                                                 std::move(block_maps[block]));
+    }
+  }
+
+  return domain;
 }
 
 std::vector<std::array<size_t, 3>> Sphere::initial_extents() const noexcept {
@@ -127,5 +250,45 @@ std::vector<std::array<size_t, 3>> Sphere::initial_extents() const noexcept {
 std::vector<std::array<size_t, 3>> Sphere::initial_refinement_levels() const
     noexcept {
   return {7, make_array<3>(initial_refinement_)};
+}
+
+std::unordered_map<std::string,
+                   std::unique_ptr<domain::FunctionsOfTime::FunctionOfTime>>
+Sphere::functions_of_time() const noexcept {
+  std::unordered_map<std::string,
+                     std::unique_ptr<domain::FunctionsOfTime::FunctionOfTime>>
+      result{};
+  if (not enable_time_dependence_) {
+    return result;
+  }
+
+  const double initial_expiration_time =
+      initial_expiration_delta_t_ ? initial_time_ + *initial_expiration_delta_t_
+                                  : std::numeric_limits<double>::infinity();
+
+  // ExpansionMap FunctionOfTime
+  result[expansion_function_of_time_name_] =
+      std::make_unique<FunctionsOfTime::PiecewisePolynomial<2>>(
+          initial_time_,
+          std::array<DataVector, 3>{
+              {{initial_expansion_}, {initial_expansion_velocity_}, {0.0}}},
+          initial_expiration_time);
+  result[expansion_function_of_time_name_ + "OuterBoundary"s] =
+      std::make_unique<FunctionsOfTime::FixedSpeedCubic>(
+          1.0, initial_time_, asymptotic_velocity_outer_boundary_,
+          decay_timescale_outer_boundary_velocity_);
+
+  // RotationAboutZAxisMap FunctionOfTime for the rotation angle about the z
+  // axis \f$\phi\f$.
+  result[rotation_about_z_axis_function_of_time_name_] =
+      std::make_unique<FunctionsOfTime::PiecewisePolynomial<3>>(
+          initial_time_,
+          std::array<DataVector, 4>{{{initial_rotation_angle_},
+                                     {initial_angular_velocity_},
+                                     {0.0},
+                                     {0.0}}},
+          initial_expiration_time);
+
+  return result;
 }
 }  // namespace domain::creators
--- src/Domain/Creators/Sphere.hpp
+++ src/Domain/Creators/Sphere.hpp
@@ -12,6 +12,7 @@
 #include "Domain/BoundaryConditions/GetBoundaryConditionsBase.hpp"
 #include "Domain/Creators/DomainCreator.hpp"  // IWYU pragma: keep
 #include "Domain/Domain.hpp"
+#include "Options/Auto.hpp"
 #include "Options/Options.hpp"
 #include "Utilities/TMPL.hpp"
 
@@ -35,11 +36,35 @@ template <typename Map1, typename Map2, typename Map3>
 class ProductOf3Maps;
 template <size_t Dim>
 class Wedge;
+namespace TimeDependent {
+template <size_t VolumeDim>
+class CubicScale;
+template <size_t VolumeDim>
+class Rotation;
+template <typename Map1, typename Map2>
+class ProductOf2Maps;
+}  // namespace TimeDependent
 }  // namespace CoordinateMaps
 
 template <typename SourceFrame, typename TargetFrame, typename... Maps>
 class CoordinateMap;
 }  // namespace domain
+namespace Sphere_detail {
+// If `Metavariables` has a `domain_parameters` member struct and
+// `domain_parameters::enable_time_dependent_maps` is `true`, then
+// inherit from `std::true_type`; otherwise, inherit from `std::false_type`.
+template <typename Metavariables, typename = std::void_t<>>
+struct enable_time_dependent_maps : std::false_type {};
+
+template <typename Metavariables>
+struct enable_time_dependent_maps<Metavariables,
+                                  std::void_t<typename Metavariables::domain>>
+    : std::bool_constant<Metavariables::domain::enable_time_dependent_maps> {};
+
+template <typename Metavariables>
+constexpr bool enable_time_dependent_maps_v =
+    enable_time_dependent_maps<Metavariables>::value;
+}  // namespace Sphere_detail
 /// \endcond
 
 namespace domain {
@@ -63,7 +88,13 @@ class Sphere : public DomainCreator<3> {
           Frame::BlockLogical, Frame::Inertial, CoordinateMaps::Wedge<3>,
           CoordinateMaps::EquatorialCompression,
           CoordinateMaps::ProductOf2Maps<CoordinateMaps::Affine,
-                                         CoordinateMaps::Identity<2>>>>;
+                                         CoordinateMaps::Identity<2>>>,
+      domain::CoordinateMap<
+          Frame::Grid, Frame::Inertial,
+          domain::CoordinateMaps::TimeDependent::CubicScale<3>,
+          domain::CoordinateMaps::TimeDependent::ProductOf2Maps<
+              domain::CoordinateMaps::TimeDependent::Rotation<2>,
+              domain::CoordinateMaps::Identity<1>>>>;
 
   struct InnerRadius {
     using type = double;
@@ -102,11 +133,111 @@ class Sphere : public DomainCreator<3> {
     using type = std::unique_ptr<BoundaryConditionsBase>;
   };
 
+  // The following options are for optional time dependent maps
+  struct TimeDependentMaps {
+    static constexpr Options::String help = {"Options for time-dependent maps"};
+  };
+
+  /// \brief The initial time of the functions of time.
+  struct InitialTime {
+    using type = double;
+    static constexpr Options::String help = {
+        "The initial time of the functions of time"};
+    using group = TimeDependentMaps;
+  };
+  /// \brief The initial time interval for updates of the functions of time.
+  struct InitialExpirationDeltaT {
+    using type = Options::Auto<double>;
+    static constexpr Options::String help = {
+        "The initial time interval for updates of the functions of time. If "
+        "Auto, then the functions of time do not expire, nor can they be "
+        "updated."};
+    using group = TimeDependentMaps;
+  };
+
+  struct ExpansionMap {
+    static constexpr Options::String help = {
+        "Options for a time-dependent CubicScale map"};
+    using group = TimeDependentMaps;
+  };
+  /// \brief The outer boundary or pivot point of the
+  /// `domain::CoordinateMaps::TimeDependent::CubicScale` map
+  struct ExpansionMapOuterBoundary {
+    using type = double;
+    static constexpr Options::String help = {
+        "Outer boundary or pivot point of the map"};
+    using group = ExpansionMap;
+    static std::string name() noexcept { return "OuterBoundary"; }
+  };
+  /// \brief The initial value of the rotation angle.
+  struct InitialExpansion {
+    using type = double;
+    static constexpr Options::String help = {
+        "Expansion value at initial time."};
+    using group = ExpansionMap;
+  };
+  /// \brief The angular velocity of the rotation.
+  struct InitialExpansionVelocity {
+    using type = double;
+    static constexpr Options::String help = {"The rate of expansion."};
+    using group = ExpansionMap;
+  };
+  /// \brief The radial velocity of the outer boundary.
+  struct AsymptoticVelocityOuterBoundary {
+    using type = double;
+    static constexpr Options::String help = {
+        "The asymptotic velocity of the outer boundary."};
+    using group = ExpansionMap;
+  };
+  /// \brief The timescale for how fast the outer boundary velocity approaches
+  /// its asymptotic value.
+  struct DecayTimescaleOuterBoundaryVelocity {
+    using type = double;
+    static constexpr Options::String help = {
+        "The timescale for how fast the outer boundary velocity approaches "
+        "its asymptotic value."};
+    using group = ExpansionMap;
+  };
+  /// \brief The name of the function of time to be added to the added to the
+  /// DataBox for the rotation-about-the-z-axis map.
+  struct ExpansionFunctionOfTimeName {
+    using type = std::string;
+    static constexpr Options::String help = {"Name of the function of time."};
+    using group = ExpansionMap;
+    static std::string name() noexcept { return "FunctionOfTimeName"; }
+  };
+
+  struct RotationAboutZAxisMap {
+    static constexpr Options::String help = {
+        "Options for a time-dependent rotation map about the z axis"};
+    using group = TimeDependentMaps;
+  };
+  /// \brief The initial value of the rotation angle.
+  struct InitialRotationAngle {
+    using type = double;
+    static constexpr Options::String help = {"Rotation angle at initial time."};
+    using group = RotationAboutZAxisMap;
+  };
+  /// \brief The angular velocity of the rotation.
+  struct InitialAngularVelocity {
+    using type = double;
+    static constexpr Options::String help = {"The angular velocity."};
+    using group = RotationAboutZAxisMap;
+  };
+  /// \brief The name of the function of time to be added to the added to the
+  /// DataBox for the rotation-about-the-z-axis map.
+  struct RotationAboutZAxisFunctionOfTimeName {
+    using type = std::string;
+    static constexpr Options::String help = {"Name of the function of time."};
+    using group = RotationAboutZAxisMap;
+    static std::string name() noexcept { return "FunctionOfTimeName"; }
+  };
+
   using basic_options = tmpl::list<InnerRadius, OuterRadius, InitialRefinement,
                                    InitialGridPoints, UseEquiangularMap>;
 
   template <typename Metavariables>
-  using options = tmpl::conditional_t<
+  using time_independent_options = tmpl::conditional_t<
       domain::BoundaryConditions::has_boundary_conditions_base_v<
           typename Metavariables::system>,
       tmpl::push_back<
@@ -115,6 +246,21 @@ class Sphere : public DomainCreator<3> {
               domain::BoundaryConditions::get_boundary_conditions_base<
                   typename Metavariables::system>>>,
       basic_options>;
+
+  using time_dependent_options =
+      tmpl::list<InitialTime, InitialExpirationDeltaT,
+                 ExpansionMapOuterBoundary, InitialExpansion,
+                 InitialExpansionVelocity, AsymptoticVelocityOuterBoundary,
+                 DecayTimescaleOuterBoundaryVelocity,
+                 ExpansionFunctionOfTimeName, InitialRotationAngle,
+                 InitialAngularVelocity, RotationAboutZAxisFunctionOfTimeName>;
+
+  template <typename Metavariables>
+  using options = tmpl::conditional_t<
+      Sphere_detail::enable_time_dependent_maps_v<Metavariables>,
+      tmpl::append<time_dependent_options,
+                   time_independent_options<Metavariables>>,
+      time_independent_options<Metavariables>>;
 
   static constexpr Options::String help{
       "Creates a 3D Sphere with seven Blocks.\n"
@@ -128,7 +274,33 @@ class Sphere : public DomainCreator<3> {
       "directions, while equidistant coordinates give better gridpoint\n"
       "spacings in the center block."};
 
+  // Constructor for time-independent version of the domain
+  // (i.e., for when
+  // Metavariables::domain::enable_time_dependent_maps == false or
+  // when the metavariables do not define
+  // Metavariables::domain::enable_time_dependent_maps)
   Sphere(typename InnerRadius::type inner_radius,
+         typename OuterRadius::type outer_radius,
+         typename InitialRefinement::type initial_refinement,
+         typename InitialGridPoints::type initial_number_of_grid_points,
+         typename UseEquiangularMap::type use_equiangular_map,
+         std::unique_ptr<domain::BoundaryConditions::BoundaryCondition>
+             boundary_condition = nullptr,
+         const Options::Context& context = {});
+
+  // Constructor for time-dependent version of the domain
+  // (i.e., for when
+  // Metavariables::domain::enable_time_dependent_maps == true),
+  // with parameters corresponding to the additional options
+  Sphere(double initial_time, std::optional<double> initial_expiration_delta_t,
+         double expansion_map_outer_boundary, double initial_expansion,
+         double initial_expansion_velocity,
+         double asymptotic_velocity_outer_boundary,
+         double decay_timescale_outer_boundary_velocity,
+         std::string expansion_function_of_time_name,
+         double initial_rotation_angle, double initial_angular_velocity,
+         std::string rotation_about_z_axis_function_of_time_name,
+         typename InnerRadius::type inner_radius,
          typename OuterRadius::type outer_radius,
          typename InitialRefinement::type initial_refinement,
          typename InitialGridPoints::type initial_number_of_grid_points,
@@ -151,7 +323,13 @@ class Sphere : public DomainCreator<3> {
   std::vector<std::array<size_t, 3>> initial_refinement_levels() const
       noexcept override;
 
+  auto functions_of_time() const noexcept -> std::unordered_map<
+      std::string,
+      std::unique_ptr<domain::FunctionsOfTime::FunctionOfTime>> override;
+
  private:
+  void check_for_parse_errors(const Options::Context& context) const;
+
   typename InnerRadius::type inner_radius_{};
   typename OuterRadius::type outer_radius_{};
   typename InitialRefinement::type initial_refinement_{};
@@ -159,6 +337,20 @@ class Sphere : public DomainCreator<3> {
   typename UseEquiangularMap::type use_equiangular_map_ = false;
   std::unique_ptr<domain::BoundaryConditions::BoundaryCondition>
       boundary_condition_;
+
+  // Variables for FunctionsOfTime options
+  bool enable_time_dependence_;
+  double initial_time_;
+  std::optional<double> initial_expiration_delta_t_;
+  double expansion_map_outer_boundary_;
+  double initial_expansion_;
+  double initial_expansion_velocity_;
+  double asymptotic_velocity_outer_boundary_;
+  double decay_timescale_outer_boundary_velocity_;
+  std::string expansion_function_of_time_name_;
+  double initial_rotation_angle_;
+  double initial_angular_velocity_;
+  std::string rotation_about_z_axis_function_of_time_name_;
 };
 }  // namespace creators
 }  // namespace domain
