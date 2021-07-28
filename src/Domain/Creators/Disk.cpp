36842c0a56a3ac9035b0f8e7c016b65bd891bcaf
Geoffrey Lovelace <geoffrey4444@gmail.com>
2021-07-28 10:53:49 -0700

    Add rotation map to disk

--- src/Domain/Creators/Disk.cpp
+++ src/Domain/Creators/Disk.cpp
@@ -4,6 +4,9 @@
 #include "Domain/Creators/Disk.hpp"
 
 #include <cmath>
+#include <cstddef>
+#include <memory>
+#include <optional>
 
 #include "Domain/Block.hpp"  // IWYU pragma: keep
 #include "Domain/BoundaryConditions/None.hpp"
@@ -12,11 +15,14 @@
 #include "Domain/CoordinateMaps/CoordinateMap.hpp"
 #include "Domain/CoordinateMaps/CoordinateMap.tpp"
 #include "Domain/CoordinateMaps/Equiangular.hpp"
+#include "Domain/CoordinateMaps/Identity.hpp"
 #include "Domain/CoordinateMaps/ProductMaps.hpp"
 #include "Domain/CoordinateMaps/ProductMaps.tpp"
+#include "Domain/CoordinateMaps/TimeDependent/Rotation.hpp"
 #include "Domain/CoordinateMaps/Wedge.hpp"
 #include "Domain/Creators/DomainCreator.hpp"  // IWYU pragma: keep
 #include "Domain/Domain.hpp"
+#include "Domain/FunctionsOfTime/PiecewisePolynomial.hpp"
 #include "Domain/Structure/BlockNeighbor.hpp"  // IWYU pragma: keep
 #include "Domain/Structure/Direction.hpp"
 #include "Domain/Structure/OrientationMap.hpp"
@@ -28,6 +34,20 @@ struct Logical;
 }  // namespace Frame
 
 namespace domain::creators {
+void Disk::check_for_parse_errors(const Options::Context& context) const {
+  using domain::BoundaryConditions::is_none;
+  if (is_none(boundary_condition_)) {
+    PARSE_ERROR(
+        context,
+        "None boundary condition is not supported. If you would like an "
+        "outflow boundary condition, you must use that.");
+  }
+  using domain::BoundaryConditions::is_periodic;
+  if (boundary_condition_ != nullptr and is_periodic(boundary_condition_)) {
+    PARSE_ERROR(context, "Cannot have periodic boundary conditions on a disk.");
+  }
+}
+
 Disk::Disk(typename InnerRadius::type inner_radius,
            typename OuterRadius::type outer_radius,
            typename InitialRefinement::type initial_refinement,
@@ -44,18 +64,45 @@ Disk::Disk(typename InnerRadius::type inner_radius,
       initial_number_of_grid_points_(                 // NOLINT
           std::move(initial_number_of_grid_points)),  // NOLINT
       use_equiangular_map_(use_equiangular_map),      // NOLINT
-      boundary_condition_(std::move(boundary_condition)) {
-  using domain::BoundaryConditions::is_none;
-  if (is_none(boundary_condition_)) {
-    PARSE_ERROR(
-        context,
-        "None boundary condition is not supported. If you would like an "
-        "outflow boundary condition, you must use that.");
-  }
-  using domain::BoundaryConditions::is_periodic;
-  if (boundary_condition_ != nullptr and is_periodic(boundary_condition_)) {
-    PARSE_ERROR(context, "Cannot have periodic boundary conditions on a disk.");
-  }
+      boundary_condition_(std::move(boundary_condition)),
+      enable_time_dependence_(false),
+      initial_time_(std::numeric_limits<double>::signaling_NaN()),
+      initial_expiration_delta_t_(std::numeric_limits<double>::signaling_NaN()),
+      initial_rotation_angle_(std::numeric_limits<double>::signaling_NaN()),
+      initial_angular_velocity_(std::numeric_limits<double>::signaling_NaN()),
+      rotation_about_z_axis_function_of_time_name_({}) {
+  check_for_parse_errors(context);
+}
+
+Disk::Disk(double initial_time,
+           std::optional<double> initial_expiration_delta_t,
+           double initial_rotation_angle, double initial_angular_velocity,
+           std::string rotation_about_z_axis_function_of_time_name,
+           typename InnerRadius::type inner_radius,
+           typename OuterRadius::type outer_radius,
+           typename InitialRefinement::type initial_refinement,
+           typename InitialGridPoints::type initial_number_of_grid_points,
+           typename UseEquiangularMap::type use_equiangular_map,
+           std::unique_ptr<domain::BoundaryConditions::BoundaryCondition>
+               boundary_condition,
+           const Options::Context& context)
+    // clang-tidy: trivially copyable
+    : inner_radius_(std::move(inner_radius)),         // NOLINT
+      outer_radius_(std::move(outer_radius)),         // NOLINT
+      initial_refinement_(                            // NOLINT
+          std::move(initial_refinement)),             // NOLINT
+      initial_number_of_grid_points_(                 // NOLINT
+          std::move(initial_number_of_grid_points)),  // NOLINT
+      use_equiangular_map_(use_equiangular_map),      // NOLINT
+      boundary_condition_(std::move(boundary_condition)),
+      enable_time_dependence_(true),
+      initial_time_(initial_time),
+      initial_expiration_delta_t_(initial_expiration_delta_t),
+      initial_rotation_angle_(initial_rotation_angle),
+      initial_angular_velocity_(initial_angular_velocity),
+      rotation_about_z_axis_function_of_time_name_(
+          rotation_about_z_axis_function_of_time_name) {
+  check_for_parse_errors(context);
 }
 
 Domain<2> Disk::create_domain() const noexcept {
@@ -126,10 +173,37 @@ Domain<2> Disk::create_domain() const noexcept {
     boundary_conditions_all_blocks.emplace_back();
   }
 
-  return Domain<2>{std::move(coord_maps),
+  Domain<2> domain{std::move(coord_maps),
                    corners,
                    {},
                    std::move(boundary_conditions_all_blocks)};
+  // Inject the hard-coded time-dependence
+  if (enable_time_dependence_) {
+    using RotationMap = domain::CoordinateMaps::TimeDependent::Rotation<2>;
+    using RotationMapForComposition =
+        domain::CoordinateMap<Frame::Grid, Frame::Inertial, RotationMap>;
+
+    constexpr size_t number_of_blocks{5};
+    std::vector<std::unique_ptr<
+        domain::CoordinateMapBase<Frame::Grid, Frame::Inertial, 2>>>
+        block_maps{number_of_blocks};
+    block_maps[0] =
+        std::make_unique<RotationMapForComposition>(RotationMapForComposition{
+            RotationMap{rotation_about_z_axis_function_of_time_name_}});
+
+    // Fill in the rest of the block maps by cloning the relevant maps
+    for (size_t block = 1; block < number_of_blocks; ++block) {
+      block_maps[block] = block_maps[0]->get_clone();
+    }
+
+    // Finally, inject the time dependent maps into the corresponding blocks
+    for (size_t block = 0; block < number_of_blocks; ++block) {
+      domain.inject_time_dependent_map_for_block(block,
+                                                 std::move(block_maps[block]));
+    }
+  }
+
+  return domain;
 }
 
 std::vector<std::array<size_t, 2>> Disk::initial_extents() const noexcept {
@@ -144,5 +218,31 @@ std::vector<std::array<size_t, 2>> Disk::initial_extents() const noexcept {
 std::vector<std::array<size_t, 2>> Disk::initial_refinement_levels()
     const noexcept {
   return {5, make_array<2>(initial_refinement_)};
+}
+
+auto Disk::functions_of_time() const noexcept -> std::unordered_map<
+    std::string, std::unique_ptr<domain::FunctionsOfTime::FunctionOfTime>> {
+  std::unordered_map<std::string,
+                     std::unique_ptr<domain::FunctionsOfTime::FunctionOfTime>>
+      result{};
+  if (not enable_time_dependence_) {
+    return result;
+  }
+  const double initial_expiration_time =
+      initial_expiration_delta_t_ ? initial_time_ + *initial_expiration_delta_t_
+                                  : std::numeric_limits<double>::infinity();
+
+  // RotationAboutZAxisMap FunctionOfTime for the rotation angle about the z
+  // axis \f$\phi\f$.
+  result[rotation_about_z_axis_function_of_time_name_] =
+      std::make_unique<FunctionsOfTime::PiecewisePolynomial<3>>(
+          initial_time_,
+          std::array<DataVector, 4>{{{initial_rotation_angle_},
+                                     {initial_angular_velocity_},
+                                     {0.0},
+                                     {0.0}}},
+          initial_expiration_time);
+
+  return result;
 }
 }  // namespace domain::creators
--- src/Domain/Creators/Disk.hpp
+++ src/Domain/Creators/Disk.hpp
@@ -6,12 +6,14 @@
 #include <array>
 #include <cstddef>
 #include <memory>
+#include <optional>
 #include <vector>
 
 #include "Domain/BoundaryConditions/BoundaryCondition.hpp"
 #include "Domain/BoundaryConditions/GetBoundaryConditionsBase.hpp"
 #include "Domain/Creators/DomainCreator.hpp"  // IWYU pragma: keep
 #include "Domain/Domain.hpp"
+#include "Options/Auto.hpp"
 #include "Options/Options.hpp"
 #include "Utilities/TMPL.hpp"
 
@@ -24,17 +26,58 @@ template <typename Map1, typename Map2>
 class ProductOf2Maps;
 template <size_t Dim>
 class Wedge;
+template <size_t VolumeDim>
+class Identity;
+namespace TimeDependent {
+template <size_t VolumeDim>
+class Rotation;
+}  // namespace TimeDependent
 }  // namespace CoordinateMaps
-
+namespace FunctionsOfTime {
+class FunctionOfTime;
+}  // namespace FunctionsOfTime
 template <typename SourceFrame, typename TargetFrame, typename... Maps>
 class CoordinateMap;
 }  // namespace domain
+
+namespace Disk_detail {
+// If `Metavariables` has a `domain_parameters` member struct and
+// `domain_parameters::enable_time_dependent_maps` is `true`, then
+// inherit from `std::true_type`; otherwise, inherit from `std::false_type`.
+template <typename Metavariables, typename = std::void_t<>>
+struct enable_time_dependent_maps : std::false_type {};
+
+template <typename Metavariables>
+struct enable_time_dependent_maps<Metavariables,
+                                  std::void_t<typename Metavariables::domain>>
+    : std::bool_constant<Metavariables::domain::enable_time_dependent_maps> {};
+
+template <typename Metavariables>
+constexpr bool enable_time_dependent_maps_v =
+    enable_time_dependent_maps<Metavariables>::value;
+}  // namespace Disk_detail
 /// \endcond
 
 namespace domain {
 namespace creators {
-/// Create a 2D Domain in the shape of a disk from a square surrounded by four
-/// wedges.
+/*!
+ * Create a 2D Domain in the shape of a disk from a square surrounded by four
+ * wedges.
+ *
+ * \note When using this domain, the
+ * metavariables struct can contain a struct named `domain`
+ * that conforms to domain::protocols::Metavariables. If
+ * domain::enable_time_dependent_maps is either set to `false`
+ * or not specified in the metavariables, then this domain will be
+ * time-independent. If domain::enable_time_dependent_maps is set
+ * to `true`, then this domain also includes a time-dependent map, along with
+ * additional options (and a corresponding constructor) for initializing the
+ * time-dependent map. These options include `InitialTime` and
+ * `InitialExpirationDeltaT`, which specify the initial time and the
+ * initial updating time interval, respectively, for the FunctionsOfTime
+ * controlling the map. The time-dependent map itself consists of a Rotation map
+ * about the z axis.
+ */
 class Disk : public DomainCreator<2> {
  public:
   using maps_list =
@@ -47,7 +90,10 @@ class Disk : public DomainCreator<2> {
                                            CoordinateMaps::Equiangular,
                                            CoordinateMaps::Equiangular>>,
                  domain::CoordinateMap<Frame::Logical, Frame::Inertial,
-                                       CoordinateMaps::Wedge<2>>>;
+                                       CoordinateMaps::Wedge<2>>,
+                 domain::CoordinateMap<
+                     Frame::Grid, Frame::Inertial,
+                     domain::CoordinateMaps::TimeDependent::Rotation<2>>>;
 
   struct InnerRadius {
     using type = double;
@@ -86,11 +132,59 @@ class Disk : public DomainCreator<2> {
     using type = std::unique_ptr<BoundaryConditionsBase>;
   };
 
+  // The following options are for optional time dependent maps
+  struct TimeDependentMaps {
+    static constexpr Options::String help = {"Options for time-dependent maps"};
+  };
+
+  /// \brief The initial time of the functions of time.
+  struct InitialTime {
+    using type = double;
+    static constexpr Options::String help = {
+        "The initial time of the functions of time"};
+    using group = TimeDependentMaps;
+  };
+  /// \brief The initial time interval for updates of the functions of time.
+  struct InitialExpirationDeltaT {
+    using type = Options::Auto<double>;
+    static constexpr Options::String help = {
+        "The initial time interval for updates of the functions of time. If "
+        "Auto, then the functions of time do not expire, nor can they be "
+        "updated."};
+    using group = TimeDependentMaps;
+  };
+
+  struct RotationAboutZAxisMap {
+    static constexpr Options::String help = {
+        "Options for a time-dependent rotation map about the z axis"};
+    using group = TimeDependentMaps;
+  };
+  /// \brief The initial value of the rotation angle.
+  struct InitialRotationAngle {
+    using type = double;
+    static constexpr Options::String help = {"Rotation angle at initial time."};
+    using group = RotationAboutZAxisMap;
+  };
+  /// \brief The angular velocity of the rotation.
+  struct InitialAngularVelocity {
+    using type = double;
+    static constexpr Options::String help = {"The angular velocity."};
+    using group = RotationAboutZAxisMap;
+  };
+  /// \brief The name of the function of time to be added to the added to the
+  /// DataBox for the rotation-about-the-z-axis map.
+  struct RotationAboutZAxisFunctionOfTimeName {
+    using type = std::string;
+    static constexpr Options::String help = {"Name of the function of time."};
+    using group = RotationAboutZAxisMap;
+    static std::string name() noexcept { return "FunctionOfTimeName"; }
+  };
+
   using basic_options = tmpl::list<InnerRadius, OuterRadius, InitialRefinement,
                                    InitialGridPoints, UseEquiangularMap>;
 
   template <typename Metavariables>
-  using options = tmpl::conditional_t<
+  using time_independent_options = tmpl::conditional_t<
       domain::BoundaryConditions::has_boundary_conditions_base_v<
           typename Metavariables::system>,
       tmpl::push_back<
@@ -100,6 +194,17 @@ class Disk : public DomainCreator<2> {
                   typename Metavariables::system>>>,
       basic_options>;
 
+  using time_dependent_options =
+      tmpl::list<InitialTime, InitialExpirationDeltaT, InitialRotationAngle,
+                 InitialAngularVelocity, RotationAboutZAxisFunctionOfTimeName>;
+
+  template <typename Metavariables>
+  using options = tmpl::conditional_t<
+      Disk_detail::enable_time_dependent_maps_v<Metavariables>,
+      tmpl::append<time_dependent_options,
+                   time_independent_options<Metavariables>>,
+      time_independent_options<Metavariables>>;
+
   static constexpr Options::String help{
       "Creates a 2D Disk with five Blocks.\n"
       "Only one refinement level for both dimensions is currently supported.\n"
@@ -108,9 +213,37 @@ class Disk : public DomainCreator<2> {
       "to the number of gridpoints along the angular dimension of the wedges.\n"
       "Equiangular coordinates give better gridpoint spacings in the angular\n"
       "direction, while equidistant coordinates give better gridpoint\n"
-      "spacings in the center block."};
+      "spacings in the center block. Note that the domain optionally includes "
+      "a time-dependent rotation map; enabling "
+      "the time-dependent map requires adding a "
+      "struct named domain to the Metavariables, with this "
+      "struct conforming to domain::protocols::Metavariables. To enable the "
+      "time-dependent map, set "
+      "Metavariables::domain::enable_time_dependent_maps to "
+      "true."};
 
+  // Constructor for time-independent version of the domain
+  // (i.e., for when
+  // Metavariables::domain::enable_time_dependent_maps == false or
+  // when the metavariables do not define
+  // Metavariables::domain::enable_time_dependent_maps)
   Disk(typename InnerRadius::type inner_radius,
+       typename OuterRadius::type outer_radius,
+       typename InitialRefinement::type initial_refinement,
+       typename InitialGridPoints::type initial_number_of_grid_points,
+       typename UseEquiangularMap::type use_equiangular_map,
+       std::unique_ptr<domain::BoundaryConditions::BoundaryCondition>
+           boundary_condition = nullptr,
+       const Options::Context& context = {});
+
+  // Constructor for time-dependent version of the domain
+  // (i.e., for when
+  // Metavariables::domain::enable_time_dependent_maps == true),
+  // with parameters corresponding to the additional options
+  Disk(double initial_time, std::optional<double> initial_expiration_delta_t,
+       double initial_rotation_angle, double initial_angular_velocity,
+       std::string rotation_about_z_axis_function_of_time_name,
+       typename InnerRadius::type inner_radius,
        typename OuterRadius::type outer_radius,
        typename InitialRefinement::type initial_refinement,
        typename InitialGridPoints::type initial_number_of_grid_points,
@@ -133,7 +266,13 @@ class Disk : public DomainCreator<2> {
   std::vector<std::array<size_t, 2>> initial_refinement_levels() const
       noexcept override;
 
+  auto functions_of_time() const noexcept -> std::unordered_map<
+      std::string,
+      std::unique_ptr<domain::FunctionsOfTime::FunctionOfTime>> override;
+
  private:
+  void check_for_parse_errors(const Options::Context& context) const;
+
   typename InnerRadius::type inner_radius_{};
   typename OuterRadius::type outer_radius_{};
   typename InitialRefinement::type initial_refinement_{};
@@ -141,6 +280,14 @@ class Disk : public DomainCreator<2> {
   typename UseEquiangularMap::type use_equiangular_map_{false};
   std::unique_ptr<domain::BoundaryConditions::BoundaryCondition>
       boundary_condition_;
+
+  // Variables for FunctionsOfTime options
+  bool enable_time_dependence_;
+  double initial_time_;
+  std::optional<double> initial_expiration_delta_t_;
+  double initial_rotation_angle_;
+  double initial_angular_velocity_;
+  std::string rotation_about_z_axis_function_of_time_name_;
 };
 }  // namespace creators
 }  // namespace domain
