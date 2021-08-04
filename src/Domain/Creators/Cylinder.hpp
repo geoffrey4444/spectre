bd199aa0574067825a179d537e8632bf0ca6533e
Geoffrey Lovelace <geoffrey4444@gmail.com>
2021-08-04 15:36:55 -0700

    Add rotational time dependence to cylinder

--- src/Domain/Creators/Cylinder.hpp
+++ src/Domain/Creators/Cylinder.hpp
@@ -5,6 +5,8 @@
 
 #include <array>
 #include <cstddef>
+#include <memory>
+#include <optional>
 #include <unordered_map>
 #include <variant>
 #include <vector>
@@ -14,6 +16,7 @@
 #include "Domain/CoordinateMaps/Distribution.hpp"
 #include "Domain/Creators/DomainCreator.hpp"  // IWYU pragma: keep
 #include "Domain/Domain.hpp"
+#include "Options/Auto.hpp"
 #include "Options/Options.hpp"
 #include "Utilities/TMPL.hpp"
 
@@ -28,24 +31,66 @@ template <typename Map1, typename Map2, typename Map3>
 class ProductOf3Maps;
 template <size_t Dim>
 class Wedge;
+template <size_t VolumeDim>
+class Identity;
+namespace TimeDependent {
+template <typename Map1, typename Map2>
+class ProductOf2Maps;
+template <size_t VolumeDim>
+class Rotation;
+}  // namespace TimeDependent
 }  // namespace CoordinateMaps
 
 template <typename SourceFrame, typename TargetFrame, typename... Maps>
 class CoordinateMap;
 }  // namespace domain
+
+namespace Cylinder_detail {
+// If `Metavariables` has a `domain_parameters` member struct and
+// `domain_parameters::enable_time_dependent_maps` is `true`, then
+// inherit from `std::true_type`; otherwise, inherit from `std::false_type`.
+template <typename Metavariables, typename = std::void_t<>>
+struct enable_time_dependent_maps : std::false_type {};
+
+template <typename Metavariables>
+struct enable_time_dependent_maps<Metavariables,
+                                  std::void_t<typename Metavariables::domain>>
+    : std::bool_constant<Metavariables::domain::enable_time_dependent_maps> {};
+
+template <typename Metavariables>
+constexpr bool enable_time_dependent_maps_v =
+    enable_time_dependent_maps<Metavariables>::value;
+}  // namespace Cylinder_detail
 /// \endcond
 
 namespace domain::creators {
-/// Create a 3D Domain in the shape of a cylinder where the cross-section
-/// is a square surrounded by four two-dimensional wedges (see `Wedge`).
-///
-/// The outer shell can be split into sub-shells and the cylinder can be split
-/// into disks along its height.
-/// The block numbering starts at the inner square and goes counter-clockwise,
-/// starting with the eastern wedge (+x-direction), through consecutive shells,
-/// then repeats this pattern for all layers bottom to top.
-///
-/// \image html Cylinder.png "The Cylinder Domain."
+/*!
+ *
+ * Create a 3D Domain in the shape of a cylinder where the cross-section
+ * is a square surrounded by four two-dimensional wedges (see `Wedge`).
+ *
+ * The outer shell can be split into sub-shells and the cylinder can be split
+ * into disks along its height.
+ * The block numbering starts at the inner square and goes counter-clockwise,
+ * starting with the eastern wedge (+x-direction), through consecutive shells,
+ * then repeats this pattern for all layers bottom to top.
+ *
+ * \image html Cylinder.png "The Cylinder Domain."
+ *
+ * \note When using this domain, the
+ * metavariables struct can contain a struct named `domain`
+ * that conforms to domain::protocols::Metavariables. If
+ * domain::enable_time_dependent_maps is either set to `false`
+ * or not specified in the metavariables, then this domain will be
+ * time-independent. If domain::enable_time_dependent_maps is set
+ * to `true`, then this domain also includes a time-dependent map, along with
+ * additional options (and a corresponding constructor) for initializing the
+ * time-dependent map. These options include `InitialTime` and
+ * `InitialExpirationDeltaT`, which specify the initial time and the
+ * initial updating time interval, respectively, for the FunctionsOfTime
+ * controlling the map. The time-dependent map itself consists of a Rotation map
+ * about the z axis.
+ */
 class Cylinder : public DomainCreator<3> {
  public:
   using maps_list = tmpl::list<
@@ -61,7 +106,12 @@ class Cylinder : public DomainCreator<3> {
       domain::CoordinateMap<
           Frame::Logical, Frame::Inertial,
           CoordinateMaps::ProductOf2Maps<CoordinateMaps::Wedge<2>,
-                                         CoordinateMaps::Affine>>>;
+                                         CoordinateMaps::Affine>>,
+      domain::CoordinateMap<
+          Frame::Grid, Frame::Inertial,
+          domain::CoordinateMaps::TimeDependent::ProductOf2Maps<
+              domain::CoordinateMaps::TimeDependent::Rotation<2>,
+              domain::CoordinateMaps::Identity<1>>>>;
 
   struct InnerRadius {
     using type = double;
@@ -183,8 +233,56 @@ class Cylinder : public DomainCreator<3> {
     using type = std::unique_ptr<BoundaryConditionsBase>;
   };
 
+  // The following options are for optional time dependent maps
+  struct TimeDependentMaps {
+    static constexpr Options::String help = {"Options for time-dependent maps"};
+  };
+
+  /// \brief The initial time of the functions of time.
+  struct InitialTime {
+    using type = double;
+    static constexpr Options::String help = {
+        "The initial time of the functions of time"};
+    using group = TimeDependentMaps;
+  };
+  /// \brief The initial time interval for updates of the functions of time.
+  struct InitialExpirationDeltaT {
+    using type = Options::Auto<double>;
+    static constexpr Options::String help = {
+        "The initial time interval for updates of the functions of time. If "
+        "Auto, then the functions of time do not expire, nor can they be "
+        "updated."};
+    using group = TimeDependentMaps;
+  };
+
+  struct RotationAboutZAxisMap {
+    static constexpr Options::String help = {
+        "Options for a time-dependent rotation map about the z axis"};
+    using group = TimeDependentMaps;
+  };
+  /// \brief The initial value of the rotation angle.
+  struct InitialRotationAngle {
+    using type = double;
+    static constexpr Options::String help = {"Rotation angle at initial time."};
+    using group = RotationAboutZAxisMap;
+  };
+  /// \brief The angular velocity of the rotation.
+  struct InitialAngularVelocity {
+    using type = double;
+    static constexpr Options::String help = {"The angular velocity."};
+    using group = RotationAboutZAxisMap;
+  };
+  /// \brief The name of the function of time to be added to the added to the
+  /// DataBox for the rotation-about-the-z-axis map.
+  struct RotationAboutZAxisFunctionOfTimeName {
+    using type = std::string;
+    static constexpr Options::String help = {"Name of the function of time."};
+    using group = RotationAboutZAxisMap;
+    static std::string name() noexcept { return "FunctionOfTimeName"; }
+  };
+
   template <typename Metavariables>
-  using options = tmpl::append<
+  using time_independent_options = tmpl::append<
       tmpl::list<InnerRadius, OuterRadius, LowerBound, UpperBound>,
       tmpl::conditional_t<
           domain::BoundaryConditions::has_boundary_conditions_base_v<
@@ -202,6 +300,17 @@ class Cylinder : public DomainCreator<3> {
           tmpl::list<IsPeriodicInZ>>,
       tmpl::list<InitialRefinement, InitialGridPoints, UseEquiangularMap,
                  RadialPartitioning, HeightPartitioning, RadialDistribution>>;
+
+  using time_dependent_options =
+      tmpl::list<InitialTime, InitialExpirationDeltaT, InitialRotationAngle,
+                 InitialAngularVelocity, RotationAboutZAxisFunctionOfTimeName>;
+
+  template <typename Metavariables>
+  using options = tmpl::conditional_t<
+      Cylinder_detail::enable_time_dependent_maps_v<Metavariables>,
+      tmpl::append<time_dependent_options,
+                   time_independent_options<Metavariables>>,
+      time_independent_options<Metavariables>>;
 
   static constexpr Options::String help{
       "Creates a right circular Cylinder with a square prism surrounded by \n"
@@ -223,8 +332,20 @@ class Cylinder : public DomainCreator<3> {
       "shell.\n"
       "Equiangular coordinates give better gridpoint spacings in the angular\n"
       "direction, while equidistant coordinates give better gridpoint\n"
-      "spacings in the center block."};
+      "spacings in the center block. Note that the domain optionally includes "
+      "a time-dependent rotation map; enabling "
+      "the time-dependent map requires adding a "
+      "struct named domain to the Metavariables, with this "
+      "struct conforming to domain::protocols::Metavariables. To enable the "
+      "time-dependent map, set "
+      "Metavariables::domain::enable_time_dependent_maps to "
+      "true."};
 
+  // Constructors for time-independent version of the domain
+  // (i.e., for when
+  // Metavariables::domain::enable_time_dependent_maps == false or
+  // when the metavariables do not define
+  // Metavariables::domain::enable_time_dependent_maps)
   Cylinder(
       double inner_radius, double outer_radius, double lower_bound,
       double upper_bound, bool is_periodic_in_z,
@@ -237,6 +358,44 @@ class Cylinder : public DomainCreator<3> {
       const Options::Context& context = {});
 
   Cylinder(
+      double inner_radius, double outer_radius, double lower_bound,
+      double upper_bound,
+      std::unique_ptr<domain::BoundaryConditions::BoundaryCondition>
+          lower_boundary_condition,
+      std::unique_ptr<domain::BoundaryConditions::BoundaryCondition>
+          upper_boundary_condition,
+      std::unique_ptr<domain::BoundaryConditions::BoundaryCondition>
+          mantle_boundary_condition,
+      const typename InitialRefinement::type& initial_refinement,
+      const typename InitialGridPoints::type& initial_number_of_grid_points,
+      bool use_equiangular_map, std::vector<double> radial_partitioning = {},
+      std::vector<double> height_partitioning = {},
+      std::vector<domain::CoordinateMaps::Distribution> radial_distribution =
+          {domain::CoordinateMaps::Distribution::Linear},
+      const Options::Context& context = {});
+
+  // Constructor for time-dependent version of the domain
+  // (i.e., for when
+  // Metavariables::domain::enable_time_dependent_maps == true),
+  // with parameters corresponding to the additional options
+  Cylinder(
+      double initial_time, std::optional<double> initial_expiration_delta_t,
+      double initial_rotation_angle, double initial_angular_velocity,
+      std::string rotation_about_z_axis_function_of_time_name,
+      double inner_radius, double outer_radius, double lower_bound,
+      double upper_bound, bool is_periodic_in_z,
+      const typename InitialRefinement::type& initial_refinement,
+      const typename InitialGridPoints::type& initial_number_of_grid_points,
+      bool use_equiangular_map, std::vector<double> radial_partitioning = {},
+      std::vector<double> height_partitioning = {},
+      std::vector<domain::CoordinateMaps::Distribution> radial_distribution =
+          {domain::CoordinateMaps::Distribution::Linear},
+      const Options::Context& context = {});
+
+  Cylinder(
+      double initial_time, std::optional<double> initial_expiration_delta_t,
+      double initial_rotation_angle, double initial_angular_velocity,
+      std::string rotation_about_z_axis_function_of_time_name,
       double inner_radius, double outer_radius, double lower_bound,
       double upper_bound,
       std::unique_ptr<domain::BoundaryConditions::BoundaryCondition>
@@ -267,7 +426,17 @@ class Cylinder : public DomainCreator<3> {
   std::vector<std::array<size_t, 3>> initial_refinement_levels()
       const noexcept override;
 
+  auto functions_of_time() const noexcept -> std::unordered_map<
+      std::string,
+      std::unique_ptr<domain::FunctionsOfTime::FunctionOfTime>> override;
+
  private:
+  void check_for_parse_errors_and_init(
+      const typename InitialRefinement::type& initial_refinement,
+      const typename InitialGridPoints::type& initial_number_of_grid_points,
+      const Options::Context& context);
+  void check_bc_parse_errors(const Options::Context& context);
+
   double inner_radius_{std::numeric_limits<double>::signaling_NaN()};
   double outer_radius_{std::numeric_limits<double>::signaling_NaN()};
   double lower_bound_{std::numeric_limits<double>::signaling_NaN()};
@@ -285,5 +454,13 @@ class Cylinder : public DomainCreator<3> {
       upper_boundary_condition_{};
   std::unique_ptr<domain::BoundaryConditions::BoundaryCondition>
       mantle_boundary_condition_{};
+
+  // Variables for FunctionsOfTime options
+  bool enable_time_dependence_;
+  double initial_time_;
+  std::optional<double> initial_expiration_delta_t_;
+  double initial_rotation_angle_;
+  double initial_angular_velocity_;
+  std::string rotation_about_z_axis_function_of_time_name_;
 };
 }  // namespace domain::creators
