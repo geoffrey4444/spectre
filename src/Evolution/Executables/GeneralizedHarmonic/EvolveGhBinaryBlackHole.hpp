4c154580d40b4619e7a83b3e9062d56d6e7cb4b7
Geoffrey Lovelace <geoffrey4444@gmail.com>
2022-05-28 09:06:26 -0700

    Do not override functions of time in BBH exec

--- src/Evolution/Executables/GeneralizedHarmonic/EvolveGhBinaryBlackHole.hpp
+++ src/Evolution/Executables/GeneralizedHarmonic/EvolveGhBinaryBlackHole.hpp
@@ -172,7 +172,7 @@ struct EvolutionMetavars {
   // Set override_functions_of_time to true to override the
   // 2nd or 3rd order piecewise polynomial functions of time using
   // `read_spec_piecewise_polynomial()`
-  static constexpr bool override_functions_of_time = true;
+  static constexpr bool override_functions_of_time = false;
 
   enum class Phase {
     Initialization,
