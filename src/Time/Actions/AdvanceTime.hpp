f76f1ecfdf77696593a62286f37038324a9bf3e2
Geoffrey Lovelace <geoffrey4444@gmail.com>
2022-09-18 15:09:42 -0700

    WIP: Print out info about time stepping each step

--- src/Time/Actions/AdvanceTime.hpp
+++ src/Time/Actions/AdvanceTime.hpp
@@ -55,9 +55,33 @@ struct AdvanceTime {
             typename ParallelComponent>
   static Parallel::iterable_action_return_t apply(
       db::DataBox<DbTags>& box, tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
-      const Parallel::GlobalCache<Metavariables>& /*cache*/,
-      const ArrayIndex& /*array_index*/, ActionList /*meta*/,
+      const Parallel::GlobalCache<Metavariables>& cache,
+      const ArrayIndex& array_index, ActionList /*meta*/,
       const ParallelComponent* const /*meta*/) {  // NOLINT const
+    auto debug_print = [&box, &array_index,
+                        &cache](const std::string& message) {
+      double min_expiration_time{std::numeric_limits<double>::max()};
+      const auto& functions_of_time = get<domain::Tags::FunctionsOfTime>(cache);
+      if (not functions_of_time.empty()) {
+        for (const auto& [name, f_of_t] : functions_of_time) {
+          if (f_of_t->time_bounds()[1] < min_expiration_time) {
+            min_expiration_time = f_of_t->time_bounds()[1];
+          }
+        }
+      }
+      Parallel::printf(
+          "AdvanceTime: i=%s texp=%1.20f t=%1.20f tstep=%1.20f tsub=%1.20f "
+          "dt=%1.20f next_trigger=%1.20f: %s\n",
+          array_index, min_expiration_time, db::get<::Tags::Time>(box),
+          db::get<::Tags::TimeStepId>(box).step_time().value(),
+          db::get<::Tags::TimeStepId>(box).substep_time().value(),
+          db::get<::Tags::TimeStep>(box).value(),
+          db::get_mutable_reference<::evolution::Tags::EventsAndDenseTriggers>(
+              make_not_null(&box))
+              .next_trigger(box),
+          message);
+    };
+    debug_print("Before"s);
     db::mutate<Tags::TimeStepId, Tags::Next<Tags::TimeStepId>, Tags::TimeStep,
                Tags::Time, Tags::Next<Tags::TimeStep>>(
         make_not_null(&box),
@@ -76,6 +100,7 @@ struct AdvanceTime {
           *time = time_id->substep_time().value();
         },
         db::get<Tags::TimeStepper<>>(box));
+    debug_print("After"s);
 
     return {Parallel::AlgorithmExecution::Continue, std::nullopt};
   }
