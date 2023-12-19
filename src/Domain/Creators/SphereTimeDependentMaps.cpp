c2e5c972d4f4662b1a44ce1ef0d1c95817050382
Geoffrey Lovelace <geoffrey4444@gmail.com>
2023-12-19 15:35:36 -0800

    HACK: expansion and rotation in sphere

--- src/Domain/Creators/SphereTimeDependentMaps.cpp
+++ src/Domain/Creators/SphereTimeDependentMaps.cpp
@@ -21,16 +21,21 @@
 #include "Domain/FunctionsOfTime/FunctionOfTime.hpp"
 #include "Domain/FunctionsOfTime/PiecewisePolynomial.hpp"
 #include "Domain/FunctionsOfTime/QuaternionFunctionOfTime.hpp"
+#include "Domain/FunctionsOfTime/SettleToConstant.hpp"
 #include "NumericalAlgorithms/SphericalHarmonics/Spherepack.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/KerrHorizon.hpp"
 #include "Utilities/ErrorHandling/Error.hpp"
 
 namespace domain::creators::sphere {
 TimeDependentMapOptions::TimeDependentMapOptions(
-    const double initial_time, const ShapeMapOptions& shape_map_options)
+    const double initial_time, const ShapeMapOptions& shape_map_options,
+    std::optional<ExpansionOptions> expansion_map_options,
+    std::optional<RotationOptions> rotation_options)
     : initial_time_(initial_time),
       initial_l_max_(shape_map_options.l_max),
-      initial_shape_values_(shape_map_options.initial_values) {}
+      initial_shape_values_(shape_map_options.initial_values),
+      expansion_map_options_(expansion_map_options),
+      rotation_options_(rotation_options) {}
 
 std::unordered_map<std::string,
                    std::unique_ptr<domain::FunctionsOfTime::FunctionOfTime>>
@@ -46,7 +51,10 @@ TimeDependentMapOptions::create_functions_of_time(
   // their initial expiration time to infinity (i.e. not expiring)
   std::unordered_map<std::string, double> expiration_times{
       {size_name, std::numeric_limits<double>::infinity()},
-      {shape_name, std::numeric_limits<double>::infinity()}};
+      {shape_name, std::numeric_limits<double>::infinity()},
+      {expansion_name, std::numeric_limits<double>::infinity()},
+      {expansion_outer_boundary_name, std::numeric_limits<double>::infinity()},
+      {rotation_name, std::numeric_limits<double>::infinity()}};
 
   // If we have control systems, overwrite these expiration times with the ones
   // supplied by the control system
@@ -101,6 +109,71 @@ TimeDependentMapOptions::create_functions_of_time(
                                  {0.0}}},
       expiration_times.at(size_name));
 
+  // Expansion function of time
+  if (expansion_map_options_.has_value()) {
+    DataVector expansion_f{
+        1, std::get<0>(expansion_map_options_.value().initial_values)};
+    DataVector expansion_df{
+        1, std::get<1>(expansion_map_options_.value().initial_values)};
+    DataVector expansion_d2f{
+        1, std::get<2>(expansion_map_options_.value().initial_values)};
+    const std::array<DataVector, 3> initial_func_and_derivs{
+        expansion_f, expansion_df, expansion_d2f};
+    const double t_match = expansion_map_options_.value().match_time;
+    const double t_decay = expansion_map_options_.value().decay_time;
+    result[expansion_name] =
+        std::make_unique<FunctionsOfTime::SettleToConstant>(
+            std::move(initial_func_and_derivs), std::move(t_match),
+            std::move(t_decay));
+  }
+
+  // Expansion outer boundary function of time
+  if (expansion_map_options_.has_value()) {
+    DataVector expansion_f{
+        1, std::get<0>(
+               expansion_map_options_.value().initial_values_outer_boundary)};
+    DataVector expansion_df{
+        1, std::get<1>(
+               expansion_map_options_.value().initial_values_outer_boundary)};
+    DataVector expansion_d2f{
+        1, std::get<2>(
+               expansion_map_options_.value().initial_values_outer_boundary)};
+    const std::array<DataVector, 3> initial_func_and_derivs{
+        expansion_f, expansion_df, expansion_d2f};
+    const double t_match = expansion_map_options_.value().match_time;
+    const double t_decay = expansion_map_options_.value().decay_time;
+    result[expansion_outer_boundary_name] =
+        std::make_unique<FunctionsOfTime::SettleToConstant>(
+            std::move(initial_func_and_derivs), std::move(t_match),
+            std::move(t_decay));
+  }
+
+  // Rotation function of time
+  if (rotation_options_.has_value()) {
+    const DataVector f{
+        get<0>(rotation_options_.value().initial_quaternion_values),
+        get<1>(rotation_options_.value().initial_quaternion_values),
+        get<2>(rotation_options_.value().initial_quaternion_values),
+        get<3>(rotation_options_.value().initial_quaternion_values)};
+    const DataVector df{
+        get<0>(rotation_options_.value().initial_quaternion_values),
+        get<1>(rotation_options_.value().initial_quaternion_values),
+        get<2>(rotation_options_.value().initial_quaternion_values),
+        get<3>(rotation_options_.value().initial_quaternion_values)};
+    const DataVector d2f{
+        get<0>(rotation_options_.value().initial_quaternion_second_derivatives),
+        get<1>(rotation_options_.value().initial_quaternion_second_derivatives),
+        get<2>(rotation_options_.value().initial_quaternion_second_derivatives),
+        get<3>(
+            rotation_options_.value().initial_quaternion_second_derivatives)};
+    const double t_match = expansion_map_options_.value().match_time;
+    const double t_decay = expansion_map_options_.value().decay_time;
+    const std::array<DataVector, 3> initial_func_and_derivs{f, df, d2f};
+    result[rotation_name] = std::make_unique<FunctionsOfTime::SettleToConstant>(
+        std::move(initial_func_and_derivs), std::move(t_match),
+        std::move(t_decay));
+  }
+
   return result;
 }
 
@@ -115,17 +188,39 @@ void TimeDependentMapOptions::build_maps(const std::array<double, 3>& center,
   shape_map_ = ShapeMap{center,         initial_l_max_,
                         initial_l_max_, std::move(transition_func),
                         shape_name,     size_name};
+  if (expansion_map_options_.has_value()) {
+    const double outer_boundary_radius =
+        expansion_map_options_.value().outer_boundary_radius;
+    const std::string exp_name{expansion_name};
+    const std::string exp_outer_name{expansion_outer_boundary_name};
+    (*expansion_map_) =
+        ExpansionMap{outer_boundary_radius, exp_name, exp_outer_name};
+  }
+  if (rotation_options_.has_value()) {
+    (*rotation_map_) = RotationMap{rotation_name};
+  }
 }
 
 // If you edit any of the functions below, be sure to update the documentation
 // in the Sphere domain creator as well as this class' documentation.
 TimeDependentMapOptions::MapType<Frame::Distorted, Frame::Inertial>
 TimeDependentMapOptions::distorted_to_inertial_map(
-    const bool include_distorted_map) {
+    const bool include_distorted_map) const {
   if (include_distorted_map) {
-    return std::make_unique<
-        IdentityForComposition<Frame::Distorted, Frame::Inertial>>(
-        IdentityMap{});
+    if (expansion_map_.has_value() and rotation_map_.has_value()) {
+      return std::make_unique<detail::di_map<ExpansionMap, RotationMap>>(
+          expansion_map_.value(), rotation_map_.value());
+    } else if (expansion_map_.has_value()) {
+      return std::make_unique<detail::di_map<ExpansionMap>>(
+          expansion_map_.value());
+    } else if (rotation_map_.has_value()) {
+      return std::make_unique<detail::di_map<RotationMap>>(
+          rotation_map_.value());
+    } else {
+      return std::make_unique<
+          IdentityForComposition<Frame::Distorted, Frame::Inertial>>(
+          IdentityMap{});
+    }
   } else {
     return nullptr;
   }
@@ -135,7 +230,7 @@ TimeDependentMapOptions::MapType<Frame::Grid, Frame::Distorted>
 TimeDependentMapOptions::grid_to_distorted_map(
     const bool include_distorted_map) const {
   if (include_distorted_map) {
-    return std::make_unique<GridToDistortedComposition>(shape_map_);
+    return std::make_unique<detail::gd_map<ShapeMap>>(shape_map_);
   } else {
     return nullptr;
   }
@@ -145,10 +240,33 @@ TimeDependentMapOptions::MapType<Frame::Grid, Frame::Inertial>
 TimeDependentMapOptions::grid_to_inertial_map(
     const bool include_distorted_map) const {
   if (include_distorted_map) {
-    return std::make_unique<GridToInertialComposition>(shape_map_);
+    if (expansion_map_.has_value() and rotation_map_.has_value()) {
+      return std::make_unique<
+          detail::gi_map<ShapeMap, ExpansionMap, RotationMap>>(
+          shape_map_, expansion_map_.value(), rotation_map_.value());
+    } else if (expansion_map_.has_value()) {
+      return std::make_unique<detail::gi_map<ShapeMap, ExpansionMap>>(
+          shape_map_, expansion_map_.value());
+    } else if (rotation_map_.has_value()) {
+      return std::make_unique<detail::gi_map<ShapeMap, RotationMap>>(
+          shape_map_, rotation_map_.value());
+    } else {
+      return std::make_unique<detail::gi_map<ShapeMap>>(shape_map_);
+    }
   } else {
-    return std::make_unique<
-        IdentityForComposition<Frame::Grid, Frame::Inertial>>(IdentityMap{});
+    if (expansion_map_.has_value() and rotation_map_.has_value()) {
+      return std::make_unique<detail::gi_map<ExpansionMap, RotationMap>>(
+          expansion_map_.value(), rotation_map_.value());
+    } else if (expansion_map_.has_value()) {
+      return std::make_unique<detail::gi_map<ExpansionMap>>(
+          expansion_map_.value());
+    } else if (rotation_map_.has_value()) {
+      return std::make_unique<detail::gi_map<RotationMap>>(
+          rotation_map_.value());
+    } else {
+      return std::make_unique<
+          IdentityForComposition<Frame::Grid, Frame::Inertial>>(IdentityMap{});
+    }
   }
 }
 }  // namespace domain::creators::sphere
--- src/Domain/Creators/SphereTimeDependentMaps.hpp
+++ src/Domain/Creators/SphereTimeDependentMaps.hpp
@@ -13,6 +13,8 @@
 
 #include "Domain/CoordinateMaps/CoordinateMap.hpp"
 #include "Domain/CoordinateMaps/Identity.hpp"
+#include "Domain/CoordinateMaps/TimeDependent/CubicScale.hpp"
+#include "Domain/CoordinateMaps/TimeDependent/Rotation.hpp"
 #include "Domain/CoordinateMaps/TimeDependent/Shape.hpp"
 #include "Domain/FunctionsOfTime/FunctionOfTime.hpp"
 #include "Options/Auto.hpp"
@@ -63,6 +65,41 @@ struct KerrSchildFromBoyerLindquist {
 // Label for shape map options
 struct Spherical {};
 
+// HACK: drop in from BinaryCompactObjectHelpers.hpp
+// should probably extract this into its own separate unit of code
+namespace detail {
+// Convenience type alias
+template <typename... Maps>
+using gi_map = domain::CoordinateMap<Frame::Grid, Frame::Inertial, Maps...>;
+template <typename... Maps>
+using gd_map = domain::CoordinateMap<Frame::Grid, Frame::Distorted, Maps...>;
+template <typename... Maps>
+using di_map =
+    domain::CoordinateMap<Frame::Distorted, Frame::Inertial, Maps...>;
+
+// Code to compute all possible map combinations for map_list
+template <typename List>
+struct power_set {
+  using rest = typename power_set<tmpl::pop_front<List>>::type;
+  using type = tmpl::append<
+      rest, tmpl::transform<rest, tmpl::lazy::push_front<
+                                      tmpl::_1, tmpl::pin<tmpl::front<List>>>>>;
+};
+template <>
+struct power_set<tmpl::list<>> {
+  using type = tmpl::list<tmpl::list<>>;
+};
+template <typename SourceFrame, typename TargetFrame, typename Maps>
+using produce_all_maps_helper =
+    tmpl::wrap<tmpl::push_front<Maps, SourceFrame, TargetFrame>,
+               domain::CoordinateMap>;
+template <typename SourceFrame, typename TargetFrame, typename... Maps>
+using produce_all_maps = tmpl::transform<
+    tmpl::remove<typename power_set<tmpl::list<Maps...>>::type, tmpl::list<>>,
+    tmpl::bind<produce_all_maps_helper, tmpl::pin<SourceFrame>,
+               tmpl::pin<TargetFrame>, tmpl::_1>>;
+}  // namespace detail
+
 /*!
  * \brief This holds all options related to the time dependent maps of the
  * domain::creators::Sphere domain creator.
@@ -81,27 +118,136 @@ struct TimeDependentMapOptions {
   using IdentityMap = domain::CoordinateMaps::Identity<3>;
   // Time-dependent maps
   using ShapeMap = domain::CoordinateMaps::TimeDependent::Shape;
+  using ExpansionMap = domain::CoordinateMaps::TimeDependent::CubicScale<3>;
+  using RotationMap = domain::CoordinateMaps::TimeDependent::Rotation<3>;
 
   template <typename SourceFrame, typename TargetFrame>
   using IdentityForComposition =
       domain::CoordinateMap<SourceFrame, TargetFrame, IdentityMap>;
-  using GridToDistortedComposition =
-      domain::CoordinateMap<Frame::Grid, Frame::Distorted, ShapeMap>;
-  using GridToInertialComposition =
-      domain::CoordinateMap<Frame::Grid, Frame::Inertial, ShapeMap>;
 
  public:
-  using maps_list =
-      tmpl::list<IdentityForComposition<Frame::Grid, Frame::Inertial>,
-                 IdentityForComposition<Frame::Grid, Frame::Distorted>,
-                 IdentityForComposition<Frame::Distorted, Frame::Inertial>,
-                 GridToDistortedComposition, GridToInertialComposition>;
+  using maps_list = tmpl::list<
+      IdentityForComposition<Frame::Grid, Frame::Inertial>,
+      IdentityForComposition<Frame::Grid, Frame::Distorted>,
+      IdentityForComposition<Frame::Distorted, Frame::Inertial>,
+      detail::produce_all_maps<Frame::Grid, Frame::Distorted, ShapeMap>,
+      detail::produce_all_maps<Frame::Grid, Frame::Inertial, ShapeMap>,
+      detail::produce_all_maps<Frame::Grid, Frame::Inertial, ShapeMap,
+                               ExpansionMap, RotationMap>,
+      detail::produce_all_maps<Frame::Grid, Frame::Inertial, ShapeMap,
+                               ExpansionMap>,
+      detail::produce_all_maps<Frame::Grid, Frame::Inertial, ShapeMap,
+                               RotationMap>,
+      detail::produce_all_maps<Frame::Distorted, Frame::Inertial, ExpansionMap,
+                               RotationMap>,
+      detail::produce_all_maps<Frame::Distorted, Frame::Inertial, ExpansionMap>,
+      detail::produce_all_maps<Frame::Distorted, Frame::Inertial, RotationMap>>;
 
   /// \brief The initial time of the functions of time.
   struct InitialTime {
     using type = double;
     static constexpr Options::String help = {
         "The initial time of the functions of time"};
+  };
+
+  struct ExpansionOptions {
+    using type = Options::Auto<ExpansionOptions, Options::AutoLabel::None>;
+    static std::string name() { return "Expansion"; }
+    static constexpr Options::String help = {
+        "Options for a time-dependent expansion map that settles to constant. "
+        "Specify 'None' to not use this map."};
+
+    struct InitialValues {
+      using type = std::array<double, 3>;
+      static constexpr Options::String help = {
+          "Initial value and 1st and second derivs of expansion."};
+    };
+    struct InitialValuesOuterBoundary {
+      using type = std::array<double, 3>;
+      static constexpr Options::String help = {
+          "Initial value and 1st and second derivs of expansion."};
+    };
+    struct OuterBoundaryRadius {
+      using type = double;
+      static constexpr Options::String help = {
+          "Radius of outer boundary used in expansion map."};
+    };
+    struct MatchTime {
+      using type = double;
+      static constexpr Options::String help = {"The match time."};
+    };
+    struct DecayTime {
+      using type = double;
+      static constexpr Options::String help = {"The decay time."};
+    };
+
+    using options = tmpl::list<InitialValues, InitialValuesOuterBoundary,
+                               OuterBoundaryRadius, MatchTime, DecayTime>;
+
+    std::array<double, 3> initial_values{
+        std::numeric_limits<double>::signaling_NaN(),
+        std::numeric_limits<double>::signaling_NaN(),
+        std::numeric_limits<double>::signaling_NaN()};
+    std::array<double, 3> initial_values_outer_boundary{
+        std::numeric_limits<double>::signaling_NaN(),
+        std::numeric_limits<double>::signaling_NaN(),
+        std::numeric_limits<double>::signaling_NaN()};
+    double outer_boundary_radius{std::numeric_limits<double>::signaling_NaN()};
+    double match_time{std::numeric_limits<double>::signaling_NaN()};
+    double decay_time{std::numeric_limits<double>::signaling_NaN()};
+  };
+
+  struct RotationOptions {
+    using type = Options::Auto<RotationOptions, Options::AutoLabel::None>;
+    static std::string name() { return "Rotation"; }
+    static constexpr Options::String help = {
+        "Options for a time-dependent rotation map that settles to constant. "
+        "Specify 'None' to not use this map."};
+    struct InitialQuaternionValues {
+      using type = std::array<double, 4>;
+      static constexpr Options::String help = {
+          "The initial quaternion values."};
+    };
+    struct InitialQuaternionFirstDerivatives {
+      using type = std::array<double, 4>;
+      static constexpr Options::String help = {
+          "The initial quaternion first derivatives."};
+    };
+    struct InitialQuaternionSecondDerivatives {
+      using type = std::array<double, 4>;
+      static constexpr Options::String help = {
+          "The initial quaternion second derivatives."};
+    };
+    struct MatchTime {
+      using type = double;
+      static constexpr Options::String help = {"The match time."};
+    };
+    struct DecayTime {
+      using type = double;
+      static constexpr Options::String help = {"The decay time."};
+    };
+
+    using options =
+        tmpl::list<InitialQuaternionValues, InitialQuaternionFirstDerivatives,
+                   InitialQuaternionSecondDerivatives, MatchTime, DecayTime>;
+
+    std::array<double, 4> initial_quaternion_values{
+        std::numeric_limits<double>::signaling_NaN(),
+        std::numeric_limits<double>::signaling_NaN(),
+        std::numeric_limits<double>::signaling_NaN(),
+        std::numeric_limits<double>::signaling_NaN()};
+    std::array<double, 4> initial_quaternion_first_derivatives{
+        std::numeric_limits<double>::signaling_NaN(),
+        std::numeric_limits<double>::signaling_NaN(),
+        std::numeric_limits<double>::signaling_NaN(),
+        std::numeric_limits<double>::signaling_NaN()};
+    std::array<double, 4> initial_quaternion_second_derivatives{
+        std::numeric_limits<double>::signaling_NaN(),
+        std::numeric_limits<double>::signaling_NaN(),
+        std::numeric_limits<double>::signaling_NaN(),
+        std::numeric_limits<double>::signaling_NaN()};
+    double match_time{std::numeric_limits<double>::signaling_NaN()};
+    double decay_time{std::numeric_limits<double>::signaling_NaN()};
   };
 
   struct ShapeMapOptions {
@@ -131,7 +277,8 @@ struct TimeDependentMapOptions {
     std::optional<std::variant<KerrSchildFromBoyerLindquist>> initial_values{};
   };
 
-  using options = tmpl::list<InitialTime, ShapeMapOptions>;
+  using options = tmpl::list<InitialTime, ShapeMapOptions, ExpansionOptions,
+                             RotationOptions>;
   static constexpr Options::String help{
       "The options for all the hard-coded time dependent maps in the Sphere "
       "domain."};
@@ -139,7 +286,9 @@ struct TimeDependentMapOptions {
   TimeDependentMapOptions() = default;
 
   TimeDependentMapOptions(double initial_time,
-                          const ShapeMapOptions& shape_map_options);
+                          const ShapeMapOptions& shape_map_options,
+                          std::optional<ExpansionOptions> expansion_map_options,
+                          std::optional<RotationOptions> rotation_options);
 
   /*!
    * \brief Create the function of time map using the options that were
@@ -173,8 +322,8 @@ struct TimeDependentMapOptions {
    * If the argument `include_distorted_map` is true, then this will be an
    * identity map. If it is false, then this returns `nullptr`.
    */
-  static MapType<Frame::Distorted, Frame::Inertial> distorted_to_inertial_map(
-      bool include_distorted_map);
+  MapType<Frame::Distorted, Frame::Inertial> distorted_to_inertial_map(
+      bool include_distorted_map) const;
 
   /*!
    * \brief This will construct the map from `Frame::Grid` to
@@ -199,6 +348,10 @@ struct TimeDependentMapOptions {
 
   inline static const std::string size_name{"Size"};
   inline static const std::string shape_name{"Shape"};
+  inline static const std::string expansion_name{"Expansion"};
+  inline static const std::string expansion_outer_boundary_name{
+      "ExpansionOuterBoundary"};
+  inline static const std::string rotation_name{"Rotation"};
 
  private:
   double initial_time_{std::numeric_limits<double>::signaling_NaN()};
@@ -206,5 +359,10 @@ struct TimeDependentMapOptions {
   ShapeMap shape_map_{};
   std::optional<std::variant<KerrSchildFromBoyerLindquist>>
       initial_shape_values_{};
+
+  std::optional<ExpansionOptions> expansion_map_options_{};
+  std::optional<RotationOptions> rotation_options_{};
+  std::optional<ExpansionMap> expansion_map_{};
+  std::optional<RotationMap> rotation_map_{};
 };
 }  // namespace domain::creators::sphere
