dc351b3498ffe00a1419ef7e4776cf794fc72220
Geoffrey Lovelace <geoffrey4444@gmail.com>
2025-08-07 13:46:03 -0700

    Add Criteria option to AH OptionTags

--- tests/Unit/ParallelAlgorithms/ApparentHorizonFinder/Test_Initialization.cpp
+++ tests/Unit/ParallelAlgorithms/ApparentHorizonFinder/Test_Initialization.cpp
@@ -48,8 +48,12 @@ SPECTRE_TEST_CASE("Unit.ApparentHorizonFinder.Initialization",
   const FastFlow expected_fast_flow{
       FastFlow::FlowType::Fast, 1.0, 0.5, 1.e-12, 1.e-2, 1.2, 5, 100};
   const ah::HorizonOptions<Frame::Grid> horizon_options{
+      std::vector<std::unique_ptr<ah::Criterion>>{},
       ylm::Strahlkorper<Frame::Grid>{4, 2.0, std::array{0.0, 0.0, 0.0}},
-      expected_fast_flow, ::Verbosity::Debug, 3, std::nullopt};
+      expected_fast_flow,
+      ::Verbosity::Debug,
+      3,
+      std::nullopt};
 
   ::Verbosity verbosity{};
   std::optional<LinkedMessageId<double>> current_time =
