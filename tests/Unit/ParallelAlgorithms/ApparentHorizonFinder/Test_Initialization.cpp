9fe1125b2020165afd2bcd7a1776a44a331d09e9
Geoffrey Lovelace <geoffrey4444@gmail.com>
2025-08-08 07:33:40 -0700

    Add tag to hold current horizon finder resolution L

--- tests/Unit/ParallelAlgorithms/ApparentHorizonFinder/Test_Initialization.cpp
+++ tests/Unit/ParallelAlgorithms/ApparentHorizonFinder/Test_Initialization.cpp
@@ -51,16 +51,18 @@ SPECTRE_TEST_CASE("Unit.ApparentHorizonFinder.Initialization",
       ylm::Strahlkorper<Frame::Grid>{4, 2.0, std::array{0.0, 0.0, 0.0}},
       expected_fast_flow, ::Verbosity::Debug, 3, std::nullopt};
 
+  std::optional<size_t> current_resolution_l{4};
   ::Verbosity verbosity{};
   std::optional<LinkedMessageId<double>> current_time =
       LinkedMessageId<double>{1.0, std::nullopt};
   FastFlow fast_flow{};
 
   ah::Initialize<MockHorizonMetavars>::apply(
-      make_not_null(&verbosity), make_not_null(&fast_flow),
-      make_not_null(&current_time), horizon_options);
+      make_not_null(&current_resolution_l), make_not_null(&verbosity),
+      make_not_null(&fast_flow), make_not_null(&current_time), horizon_options);
 
   CHECK(verbosity == ::Verbosity::Debug);
   CHECK_FALSE(current_time.has_value());
   CHECK(fast_flow == expected_fast_flow);
+  CHECK_FALSE(current_resolution_l.has_value());
 }
