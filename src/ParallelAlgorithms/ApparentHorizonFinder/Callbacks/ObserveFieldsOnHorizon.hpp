2573749abfac518a70c2c82121f67ed379287a81
Geoffrey Lovelace <geoffrey4444@gmail.com>
2025-10-17 16:37:01 +0000

    WIP: Cap horizon observers to MaxOutputLForShape

--- src/ParallelAlgorithms/ApparentHorizonFinder/Callbacks/ObserveFieldsOnHorizon.hpp
+++ src/ParallelAlgorithms/ApparentHorizonFinder/Callbacks/ObserveFieldsOnHorizon.hpp
@@ -12,6 +12,7 @@
 #include <utility>
 #include <vector>
 
+#include "ControlSystem/Tags/SystemTags.hpp"
 #include "DataStructures/DataBox/DataBox.hpp"
 #include "DataStructures/DataBox/TagName.hpp"
 #include "DataStructures/LinkedMessageId.hpp"
@@ -32,6 +33,7 @@
 #include "ParallelAlgorithms/ApparentHorizonFinder/FastFlow.hpp"
 #include "ParallelAlgorithms/ApparentHorizonFinder/Protocols/Callback.hpp"
 #include "ParallelAlgorithms/ApparentHorizonFinder/Tags.hpp"
+#include "Utilities/ErrorHandling/Error.hpp"
 #include "Utilities/Gsl.hpp"
 #include "Utilities/PrettyType.hpp"
 #include "Utilities/ProtocolHelpers.hpp"
@@ -137,9 +139,27 @@ struct ObserveFieldsOnHorizon : tt::ConformsTo<ah::protocols::Callback> {
     // coefficients regardless of the current value of l_max and (b) write a
     // constant number of columns for each row of data regardless of the current
     // l_max.
+    size_t max_l_to_write = strahlkorper.l_max();
+    if constexpr (Parallel::is_in_global_cache<
+                      Metavariables,
+                      control_system::Tags::MaxOutputLForShape>) {
+      const auto& max_output_l =
+          Parallel::get<control_system::Tags::MaxOutputLForShape>(cache);
+      if (max_output_l.has_value()) {
+        if (UNLIKELY(*max_output_l < strahlkorper.l_max())) {
+          ERROR("The option MaxOutputLForShape ("
+                << *max_output_l << ") is smaller than the current "
+                << "Strahlkorper resolution L=" << strahlkorper.l_max()
+                << ". Increase MaxOutputLForShape or the horizon criteria "
+                << "maximum L to avoid truncating data.");
+        }
+        max_l_to_write = *max_output_l;
+      }
+    }
+
     ylm::fill_ylm_legend_and_data(make_not_null(&ylm_legend),
                                   make_not_null(&ylm_data), strahlkorper,
-                                  current_time.id, strahlkorper.l_max());
+                                  current_time.id, max_l_to_write);
 
     const std::string ylm_subfile_name{std::string{"/"} + surface_name +
                                        "_Ylm"};
--- src/ParallelAlgorithms/Interpolation/Callbacks/ObserveSurfaceData.hpp
+++ src/ParallelAlgorithms/Interpolation/Callbacks/ObserveSurfaceData.hpp
@@ -8,6 +8,7 @@
 #include <utility>
 #include <vector>
 
+#include "ControlSystem/Tags/SystemTags.hpp"
 #include "DataStructures/DataBox/DataBox.hpp"
 #include "DataStructures/DataBox/TagName.hpp"
 #include "IO/H5/TensorData.hpp"
@@ -27,6 +28,7 @@
 #include "Parallel/Reduction.hpp"
 #include "ParallelAlgorithms/Interpolation/InterpolationTargetDetail.hpp"
 #include "ParallelAlgorithms/Interpolation/Protocols/PostInterpolationCallback.hpp"
+#include "Utilities/ErrorHandling/Error.hpp"
 #include "Utilities/Functional.hpp"
 #include "Utilities/Gsl.hpp"
 #include "Utilities/PrettyType.hpp"
@@ -158,9 +160,27 @@ struct ObserveSurfaceData
     // coefficients regardless of the current value of l_max and (b) write a
     // constant number of columns for each row of data regardless of the current
     // l_max.
+    size_t max_l_to_write = strahlkorper.l_max();
+    if constexpr (Parallel::is_in_global_cache<
+                      Metavariables,
+                      control_system::Tags::MaxOutputLForShape>) {
+      const auto& max_output_l =
+          Parallel::get<control_system::Tags::MaxOutputLForShape>(cache);
+      if (max_output_l.has_value()) {
+        if (UNLIKELY(*max_output_l < strahlkorper.l_max())) {
+          ERROR("The option MaxOutputLForShape ("
+                << *max_output_l << ") is smaller than the current "
+                << "Strahlkorper resolution L=" << strahlkorper.l_max()
+                << ". Increase MaxOutputLForShape or the horizon criteria "
+                << "maximum L to avoid truncating data.");
+        }
+        max_l_to_write = *max_output_l;
+      }
+    }
+
     ylm::fill_ylm_legend_and_data(make_not_null(&ylm_legend),
                                   make_not_null(&ylm_data), strahlkorper, time,
-                                  strahlkorper.l_max());
+                                  max_l_to_write);
 
     const std::string ylm_subfile_name{std::string{"/"} + surface_name +
                                        "_Ylm"};
