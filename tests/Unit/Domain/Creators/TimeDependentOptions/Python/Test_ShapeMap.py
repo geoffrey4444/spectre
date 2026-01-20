a07322b457c34adce8d3265bc6245824387a192c
Geoffrey Lovelace <geoffrey4444@gmail.com>
2026-01-20 22:52:28 +0000

    Add time dependent shape map variant: auto value, manual derivatives

--- tests/Unit/Domain/Creators/TimeDependentOptions/Python/Test_ShapeMap.py
+++ tests/Unit/Domain/Creators/TimeDependentOptions/Python/Test_ShapeMap.py
@@ -28,6 +28,22 @@ class TestShapeMap(unittest.TestCase):
         self.assertNotEqual(shape_map_C, None)
         self.assertNotEqual(shape_map_no_object, None)
 
+    def test_size_value_and_derivatives(self):
+        shape_map_A = ShapeMapOptions["A"](
+            10,
+            KerrSchildFromBoyerLindquist(1.0, [0.0, 0.0, 0.99]),
+            None,
+            [1.2, 2.3],
+        )
+        shape_map_B = ShapeMapOptions["B"](
+            10,
+            KerrSchildFromBoyerLindquist(1.0, [0.0, 0.0, 0.99]),
+            0.5,
+            [1.0, 2.4],
+        )
+        self.assertNotEqual(shape_map_A, None)
+        self.assertNotEqual(shape_map_B, None)
+
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
